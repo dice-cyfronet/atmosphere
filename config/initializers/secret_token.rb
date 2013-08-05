fa20923f5be5b7452cf8ebf9cf14a280f541f4b3
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-08-05 17:01:39 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Air::Application.config.secret_key_base = '890d54e1ca8d78a34bf238d244ba7a84be79287049015184ad6f4cb3999b611a82147cda7b777d34308d12bae309fe5f64e43276c0df93bf0a3396e92f055946'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Air::Application.config.session_store :cookie_store, key: '_air_session'
