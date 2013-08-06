4ee4bb761f5e29b2d102b64f976cb48b885739b8
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-08-06 20:13:12 +0200

    login using login or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable,
          :rememberable, :trackable, :recoverable,
          :validatable, :token_authenticatable
+
+  # Devise method overridden to allow sing in with email or login
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(login) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
