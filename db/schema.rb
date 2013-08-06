1cc24fb5cf25171378c338a9a5c22f5dab1cb293
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-08-06 21:01:35 +0200

    Recoverable section uncommented - required to reset password

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,18 @@
 ActiveRecord::Schema.define(version: 20130806144744) do
 
   create_table "users", force: true do |t|
-    t.string   "login",                default: "", null: false
-    t.string   "encrypted_password",   default: "", null: false
+    t.string   "login",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",        default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "authentication_token"
-    t.string   "email",                default: "", null: false
+    t.string   "email",                  default: "", null: false
     t.string   "full_name"
     t.datetime "created_at"
     t.datetime "updated_at"
