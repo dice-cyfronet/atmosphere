f8f59e5aeba167f9b35305ce79c90994ee184618
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-08-06 14:30:55 +0200

    handling CanCan::AccessDenied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,18 @@ class ApplicationController < ActionController::Base
     method = "#{resource}_params"
     params[resource] &&= send(method) if respond_to?(method, true)
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    if current_user.nil?
+      session[:next] = request.fullpath
+      puts session[:next]
+      redirect_to new_user_session_path, :alert => t('devise.failure.unauthenticated')
+    else
+      if request.env["HTTP_REFERER"].present?
+        redirect_to :back, :alert => exception.message
+      else
+        redirect_to root_url, :alert => exception.message
+      end
+    end
+  end
 end
