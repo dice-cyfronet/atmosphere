843d03603a80c9241a684d353368e8e7f1fadfe6
tomek <tomek.bartynski@gmail.com>
2013-08-21 14:02:59 +0200

    added association for templates saved from vm, ref #1970

--- app/models/virtual_machine.rb
+++ app/models/virtual_machine.rb
@@ -1,2 +1,3 @@
 class VirtualMachine < ActiveRecord::Base
+  has_many :saved_templates, :class_name: 'VirtualMachineTemplate'
 end
--- app/models/virtual_machine_template.rb
+++ app/models/virtual_machine_template.rb
@@ -1,2 +1,3 @@
 class VirtualMachineTemplate < ActiveRecord::Base
+  belongs_to :source_vm, :class_name: 'VirtualMachine', :foreign_key: 'virtual_machine_id'
 end
