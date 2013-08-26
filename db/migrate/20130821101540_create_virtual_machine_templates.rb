3a5913317b1cd1b7daaa389a208bc52fdb23f431
tomek <tomek.bartynski@gmail.com>
2013-08-26 15:01:15 +0200

    added association between vm tmpl and appliance type

--- db/migrate/20130821101540_create_virtual_machine_templates.rb
+++ db/migrate/20130821101540_create_virtual_machine_templates.rb
@@ -7,11 +7,13 @@ class CreateVirtualMachineTemplates < ActiveRecord::Migration
 
       t.references :compute_site,         null:false
       t.references :virtual_machine
+      t.references :appliance_type
 
       t.timestamps
     end
 
     add_foreign_key :virtual_machine_templates, :compute_sites
     add_foreign_key :virtual_machine_templates, :virtual_machines
+    add_foreign_key :virtual_machine_templates, :appliance_types
   end
 end
