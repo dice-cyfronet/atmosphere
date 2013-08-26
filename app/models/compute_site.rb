1f04dc74ac91432592cf13d2f8156ea9680a66a6
tomek <tomek.bartynski@gmail.com>
2013-08-26 17:40:19 +0200

    allowed nil value for openstack-specific field that is being validated

--- app/models/compute_site.rb
+++ app/models/compute_site.rb
@@ -14,9 +14,12 @@
 class ComputeSite < ActiveRecord::Base
   extend Enumerize
 
+  validates_presence_of :site_id, :site_type
   enumerize :site_type, in: [:public, :private], predicates: true
   validates :site_type, inclusion: %w(public private)
-  validates :auth_method, inclusion: %w(password key rax-kskey)
+
+  # openstack specific
+  validates :auth_method, inclusion: %w(password key rax-kskey), :allow_nil => true
   validates :technology, inclusion: %w(openstack amazon)
 
   has_many :virtual_machines
