fe59bbd96a910f9000bcfc2a8e6a305d9a75c36c
gubala <t.gubala@cyfronet.pl>
2013-08-26 11:50:27 +0200

    syntax error in seeds.db fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-admin = User.create(login: 'admin', full_name: 'Root Admiński z Superuserów', email: 'admin@localhost.pl', password: 'airtraffic123', password_confirmation: 'airtraffic123', authentication_token: 'secret' roles: [:admin, :developer])
+admin = User.create(login: 'admin', full_name: 'Root Admiński z Superuserów', email: 'admin@localhost.pl', password: 'airtraffic123', password_confirmation: 'airtraffic123', authentication_token: 'secret', roles: [:admin, :developer])
 
 ApplianceSet.create(name: 'test appliance set', user: admin)
 
