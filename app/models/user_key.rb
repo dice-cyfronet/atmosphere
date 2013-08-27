344fe21cadae3847380e8bc69e770170be483620
tomek <tomek.bartynski@gmail.com>
2013-08-27 15:44:48 +0200

    added tests and validations to user key

--- app/models/user_key.rb
+++ app/models/user_key.rb
@@ -1,3 +1,31 @@
+require 'tempfile'
+
 class UserKey < ActiveRecord::Base
+
+  FINGER_PRINT_RE = /([\d\h]{2}:)+[\d\h]{2}/
+
+  validates_presence_of :name, :public_key, :fingerprint
+  attr_readonly :name, :public_key, :fingerprint
+  before_create :generate_fingerprint
   belongs_to :user
+
+  private
+  def generate_fingerprint
+    logger.info "Generating fingerprint for #{public_key}"
+    fprint = nil
+    Tempfile.open('ssh_public_key', '/tmp') do |file|
+      file.puts self.public_key
+      file.rewind
+      output = nil
+      IO.popen("ssh-keygen -lf #{file.path}") {|out|
+        output = out.read
+      }
+      if output
+        logger.info output
+        fprint = output.gsub(FINGER_PRINT_RE).first
+      end
+    end
+    logger.info "Fingerprint #{fprint}"
+    self.fingerprint = fprint
+  end
 end
