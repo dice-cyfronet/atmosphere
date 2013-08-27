bb539a51ce4058a74d1344d53c707a5f2bb07d90
gubala <t.gubala@cyfronet.pl>
2013-08-27 12:39:12 +0200

    small code changes for model coding consistency ;)

--- app/models/appliance.rb
+++ app/models/appliance.rb
@@ -13,17 +13,12 @@
 class Appliance < ActiveRecord::Base
 
   belongs_to :appliance_set
-  # This should also make sure the referenced entity exists; but we still should make a foreign key constraint in DB
-  validates :appliance_set, presence: true
-
   belongs_to :appliance_type
-  validates :appliance_type, presence: true
-
   belongs_to :appliance_configuration_instance
-  validates :appliance_configuration_instance, presence: true
+
+  validates_presence_of :appliance_set, :appliance_type, :appliance_configuration_instance
 
   has_many :http_mappings, dependent: :destroy
-
   has_and_belongs_to_many :virtual_machines
 
 end
