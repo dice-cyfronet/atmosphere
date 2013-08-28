8e54f05835466875f22c25bd4322cf7ae861f8b6
tomek <tomek.bartynski@gmail.com>
2013-08-28 10:10:31 +0200

    corrected syntax error

--- spec/factories.rb
+++ spec/factories.rb
@@ -55,6 +55,7 @@ FactoryGirl.define do
     public_key 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAklOUpkDHrfHY17SbrmTIpNLTGK9Tjom/BWDSUGPl+nafzlHDTYW7hdI4yZ5ew18JH4JW9jbhUFrviQzM7xlELEVf4h9lFX5QVkbPppSwg0cda3Pbv7kOdJ/MTyBlWXFCR+HAo3FXRitBqxiX1nKhXpHAZsMciLq8V6RjsNAQwdsdMFvSlVK/7XAt3FaoJoAsncM1Q9x5+3V0Ww68/eIFmb1zuUFljQJKprrX88XypNDvjYNby6vw/Pb0rwert/EnmZ+AW4OZPnTPI89ZPmVMLuayrD2cE86Z/il8b+gw3r3+1nKatmIkjn2so1d01QraTlMqVSsbxNrRFi9wrf+M7Q== factorized@sting'
     fingerprint 'rubbish! Real fingrprint is calculated in after_initialize method in model'
     user
+  end
 
   factory :port_mapping_template do |f|
     service_name { Faker::Lorem.word }
@@ -71,4 +72,5 @@ FactoryGirl.define do
     appliance_configuration_instance
     appliance_type
   end
+
 end
