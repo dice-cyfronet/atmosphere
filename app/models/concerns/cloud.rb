8025846d9ddfd8190ca50b28de6a72714214b32f
tomek <tomek.bartynski@gmail.com>
2013-09-24 16:04:44 +0200

    added a method for getting cloud client for given cloud site

---
+++ app/models/concerns/cloud.rb
@@ -0,0 +1,13 @@
+module Cloud extend ActiveSupport::Concern
+
+  module ClassMethods
+
+    def get_cloud_client_for_site(site_id)
+      cloud_site_conf = Air::Application.config.cloud_sites[site_id]
+      @cloud_client = Fog::Compute.new(cloud_site_conf)
+      logger.info "Returning cloud client #{@cloud_client.class}"
+      @cloud_client
+    end
+
+  end
+end
