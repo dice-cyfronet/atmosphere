1bf371ee1076e9f1938955c90f424d551ffbb382
gubala <t.gubala@cyfronet.pl>
2013-09-24 14:57:14 +0200

    some more functions for traditional appliance type view

--- app/controllers/admin/appliance_types_controller.rb
+++ app/controllers/admin/appliance_types_controller.rb
@@ -7,7 +7,22 @@ class Admin::ApplianceTypesController < ApplicationController
 
   # GET /admin/appliance_types/1
   def show
-    logger.debug @appliance_type.name
+    render 'admin/appliance_types/show', layout: false
+  end
+
+  # GET /admin/appliance_types/new
+  def new
+    render 'admin/appliance_types/new', layout: false
+  end
+
+  # POST /admin/appliance_types
+  def create
+    if @appliance_type.save#(appliance_type_params)
+      #redirect_to [:admin, @appliance_type], notice: 'Compute site was successfully created.'
+      render 'admin/appliance_types/show', layout: false
+    else
+      render 'admin/appliance_types/new', layout: false
+    end
   end
 
   # GET /admin/appliance_types/1/edit
@@ -17,7 +32,7 @@ class Admin::ApplianceTypesController < ApplicationController
   # PATCH/PUT /admin/appliance_types/1
   def update
     if @appliance_type.update(appliance_type_params)
-      redirect_to [:admin, @appliance_set], notice: 'ApplianceType was successfully updated.'
+      redirect_to [:admin, @appliance_type], notice: 'ApplianceType was successfully updated.'
     else
       render action: 'edit'
     end
@@ -34,7 +49,7 @@ class Admin::ApplianceTypesController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def appliance_type_params
       params.require(:appliance_type).permit(
-        :name, :description, :visibility, :shared, :scalable, :author,
+        :name, :description, :visibility, :shared, :scalable, :user_id,
         :preference_memory, :preference_disk,  :preference_cpu)
     end
 end
