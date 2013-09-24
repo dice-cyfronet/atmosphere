6e96f653ecc9275fe4cf431db0300abc84eb9f0e
tomek <tomek.bartynski@gmail.com>
2013-09-24 21:06:04 +0200

    code cleaning only

--- config/initializers/unify_fog_cloud_clients.rb
+++ config/initializers/unify_fog_cloud_clients.rb
@@ -7,4 +7,7 @@ class Fog::Compute::OpenStack::Real
   def import_key_pair(name, public_key)
     create_key_pair(name, public_key)
   end
+
+  # alias does not work since create_key_pair method is also added using magic
+  # alias :import_key_pair :create_key_pair
 end
