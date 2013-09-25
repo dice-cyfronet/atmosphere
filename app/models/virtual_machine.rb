19d0191fb5e093f96fb162ea5e2c61a51758ad56
tomek <tomek.bartynski@gmail.com>
2013-09-25 15:42:15 +0200

    added support for user key and user data when creating a vm

--- app/models/virtual_machine.rb
+++ app/models/virtual_machine.rb
@@ -22,7 +22,7 @@ class VirtualMachine < ActiveRecord::Base
   has_and_belongs_to_many :appliances
   validates_presence_of :name, :virtual_machine_template_id
   validates_uniqueness_of :id_at_site, :scope => :compute_site_id
-
+  
   before_create :instantiate_vm
   before_destroy :delete_in_cloud
 
@@ -36,7 +36,12 @@ class VirtualMachine < ActiveRecord::Base
     logger.info 'Instantiating'
     vm_tmpl = VirtualMachineTemplate.find(virtual_machine_template_id)
     cloud_client = VirtualMachine.get_cloud_client_for_site(vm_tmpl.compute_site.site_id)
-    server = cloud_client.servers.create(:flavor_ref => 1, :name => name, :image_ref => vm_tmpl.id_at_site)
+    servers_params = {:flavor_ref => 1, :name => name, :image_ref => vm_tmpl.id_at_site, :key_name => 'tomek'}
+    unless appliances.blank?
+      user_data = appliances.first.appliance_configuration_instance.payload
+      servers_params[:user_data] = user_data if user_data
+    end
+    server = cloud_client.servers.create(servers_params)
     logger.info "instantiated #{server.id}"
     self[:id_at_site] = server.id
     self[:compute_site_id] = vm_tmpl.compute_site_id
