8bc65550bae66d1d5bf577bf33ab13b7623ccf6b
gubala <t.gubala@cyfronet.pl>
2013-10-14 14:08:21 +0200

    replaced PMT handling with nested resource routing

--- app/controllers/admin/port_mapping_templates_controller.rb
+++ app/controllers/admin/port_mapping_templates_controller.rb
@@ -1,69 +1,61 @@
 class Admin::PortMappingTemplatesController < ApplicationController
 
-  load_and_authorize_resource :port_mapping_template
-  #before_filter :set_appliance_types#, only: [:index, :show, :new, :create, :destroy]
+  load_and_authorize_resource :appliance_type
+  load_and_authorize_resource :port_mapping_template, through: :appliance_type#, shallow: true
+  layout false
 
   # GET /admin/port_mapping_templates
   def index
-    @appliance_type = ApplianceType.find(params[:appliance_type_id])
-    render partial: 'index', layout: false
+    render partial: 'index'
   end
 
   # GET /admin/port_mapping_templates/1
   def show
-    render partial: 'show', layout: false
+    render partial: 'show'
   end
 
   # GET /admin/port_mapping_templates/new
   def new
-    @appliance_type = ApplianceType.find(params[:appliance_type_id])
-    render partial: 'edit', layout: false
+    render partial: 'edit'
   end
 
   # POST /admin/port_mapping_templates
   def create
-    @appliance_type = ApplianceType.find(params[:port_mapping_template][:appliance_type_id])
     if @port_mapping_template.save port_mapping_template_params
       @notice = 'Appliance Type was successfully added.'
     else
       @alert = @port_mapping_template.errors.full_messages.join('</br>')
     end
-    render partial: 'index', layout: false
+    render partial: 'index'
   end
 
   # GET /admin/port_mapping_templates/1/edit
   def edit
-    render partial: 'edit', layout: false
+    render partial: 'edit'
   end
 
   # PATCH/PUT /admin/port_mapping_templates/1
   def update
-    @appliance_type = @port_mapping_template.appliance_type
     if @port_mapping_template.update port_mapping_template_params
       @notice = 'Appliance Type was successfully updated.'
     else
       @alert = @port_mapping_template.errors.full_messages.join('</br>')
     end
-    render partial: 'index', layout: false
+    render partial: 'index'
   end
 
   # DELETE /admin/port_mapping_templates/1.json
   def destroy
-    @appliance_type = @port_mapping_template.appliance_type
     if @port_mapping_template.destroy
       @notice = 'Port Mapping was successfully removed.'
     else
       @alert = @port_mapping_template.errors.full_messages.join('</br>')
     end
-    render partial: 'index', layout: false
+    render partial: 'index'
   end
 
 
   private
-
-    def set_appliance_type
-      #@appliance_type = (@port_mapping_template (@appliance_types ? @appliance_types : ApplianceType.all).def_order
-    end
 
     # Only allow a trusted parameter "white list" through.
     def port_mapping_template_params
