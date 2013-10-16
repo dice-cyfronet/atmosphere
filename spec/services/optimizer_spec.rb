4461b3e2c6e569f01c1664c2173931540cc2f86e
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-10-16 20:05:21 +0200

    don't addign values into variables in context, use let instead

--- spec/services/optimizer_spec.rb
+++ spec/services/optimizer_spec.rb
@@ -11,21 +11,21 @@ describe Optimizer do
   it 'is not nil' do
      expect(subject).not_to be_nil
   end
-  
+
   context 'new appliance created' do
 
-    wf = FactoryGirl.create(:workflow_appliance_set)
+    let!(:wf) { create(:workflow_appliance_set) }
 
 
     context 'vm can be shared' do
-      shareable_appl_type = FactoryGirl.create(:shareable_appliance_type)
-      
+      let!(:shareable_appl_type) { create(:shareable_appliance_type) }
+
 
       context 'vm cannot be reused' do
 
         it 'instantiates a new vm' do
           appl = Appliance.create(appliance_set: wf, appliance_type: shareable_appl_type, appliance_configuration_instance: FactoryGirl.create(:appliance_configuration_instance))
-          
+
         end
 
       end
@@ -33,14 +33,14 @@ describe Optimizer do
       context 'vm can be reused' do
 
         it 'reuses available vm' do
-        
+
         end
 
       end
     end
 
     context 'vm cannot be shared' do
-      
+
       it 'instantiates a new vm' do
 
       end
