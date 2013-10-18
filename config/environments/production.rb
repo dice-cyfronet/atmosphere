329c3ea50bca5a31b811f63b173a260a88ba39e7
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-10-18 12:57:01 +0200

    assets compile on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Air::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -85,4 +85,6 @@ Air::Application.configure do
       resource '/api/v1/*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
     end
   end
+
+  config.ember.variant = :production
 end
