2236fac1e902213df388977a9ab0a9bdc9d4d625
tomek <tomek.bartynski@gmail.com>
2013-10-24 16:42:28 +0200

    worker invokes DNAT using Faraday-based client

--- app/workers/wrangler_registrar_worker.rb
+++ app/workers/wrangler_registrar_worker.rb
@@ -1,18 +1,21 @@
+require 'wrangler'
+
 class WranglerRegistrarWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: :wrangler
+  sidekiq_options queue: :wrangler_registrar
 
-  def perform
-
+  def perform(ip, protocol, port)
+    add(ip, protocol, port)
   end
-
-  # conn = Faraday.new(url: 'http://10.100.0.24:8400') do |faraday|
-  #   faraday.request :url_encoded
-  #   faraday.response :logger 
-  #   faraday.adapter Faraday.default_adapter
-  #   faraday.basic_auth(user,passwd)
-  # end
-  # conn,get '/dnat'
+  
+  def add(ip, protocol, port)
+    dnat_client = Wrangler::Client.dnat_client
+    resp = dnat_client.post do |req|
+      req.url "/dnat/#{ip}"
+      req.headers['Content-Type'] = 'application/json'
+      req.body = JSON.generate [{proto: protocol, port: port}]
+    end
+  end
 
 end
--- spec/workers/wrangler_registrar_worker_spec.rb
+++ spec/workers/wrangler_registrar_worker_spec.rb
@@ -8,7 +8,34 @@ describe WranglerRegistrarWorker do
     end
   end
 
-  it 'calls remote wrangler service' do
+  PRIV_IP = '10.1.2.3'
+  PUB_IP = '149.156.9.5'
+  PROTO = 'tcp'
+  PRIV_PORT = 8888
+  PUB_PORT = 11921
+
+  context 'using wrangler client' do
+    stubs = nil
+    before do
+      stubs = Faraday::Adapter::Test::Stubs.new do |stub|
+        stub.post("/dnat/#{PRIV_IP}", JSON.generate([{proto: PROTO, port: PRIV_PORT}])) {[200, {}, "[{\"privIp\":\"#{PRIV_IP}\",\"pubPort\":#{PUB_PORT},\"proto\":\"#{PROTO}\",\"privPort\":#{PRIV_PORT},\"pubIp\":\"#{PUB_IP}\"}]"]}
+      end
+      stubbed_dnat_client = Faraday.new do |builder|
+        builder.adapter :test, stubs
+      end
+      Wrangler::Client.stub(:dnat_client).and_return stubbed_dnat_client
+    end
+    it 'calls remote wrangler service' do
+      subject.perform(PRIV_IP, PROTO, PRIV_PORT)
+      stubs.verify_stubbed_calls
+    end
+  end
+
+  it 'raises error for invliad protcol' do
+    pending
+  end
+
+  it 'raises error for forbidden port number' do
     pending
   end
 
