cfa2d8116196f964bda07409c61489af9ed54c0a
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-10-24 11:20:59 +0200

    mailer configuration

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,8 +1,13 @@
 unless Air.config.exception_notification.blank?
+  Air::Application.config.action_mailer.delivery_method = :sendmail
+  Air::Application.config.action_mailer.perform_deliveries = true
+  Air::Application.config.action_mailer.raise_delivery_errors = true
+  Air::Application.config.action_mailer.default_url_options = { host: Air.config.mailer.host }
+
   Air::Application.config.middleware.use ExceptionNotification::Rack,
-    :email => {
-      :email_prefix => Air.config.exception_notification.email_prefix,
-      :sender_address => Air.config.exception_notification.sender_address,
-      :exception_recipients => Air.config.exception_notification.exception_recipients
+    email: {
+      email_prefix: Air.config.exception_notification.email_prefix,
+      sender_address: Air.config.exception_notification.sender_address,
+      exception_recipients: Air.config.exception_notification.exception_recipients
     }
 end
