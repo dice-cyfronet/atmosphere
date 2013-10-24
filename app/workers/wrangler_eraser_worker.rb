1164babb64b15f4ef103ff76bd842561c82b874c
tomek <tomek.bartynski@gmail.com>
2013-10-24 16:44:06 +0200

    scaffold of the worker that removes entries from wrangler

---
+++ app/workers/wrangler_eraser_worker.rb
@@ -0,0 +1,21 @@
+require 'wrangler'
+
+class WranglerEraserWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :wrangler_eraser
+
+  def perform
+
+  end
+  
+  def remove(ip, port = nil, protocol = nil)
+    dnat_client = Wrangler::Client.dnat_client
+    resp = dnat_client.delete(build_path_for_params(ip, port, protocol))
+  end
+
+  def build_path_for_params(ip, port, protocol)
+    "/dnat/#{ip}#{'/' + port.to_s if port}#{'/' + protocol if protocol}"
+  end
+
+end
