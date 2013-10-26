8b2241d35f72098a4268298083d771d89e88056b
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-10-26 22:43:10 +0200

    small refactoring - maybe appliance method should be moved into separate class (?)

--- app/services/http_mapping_proxy_conf_generator.rb
+++ app/services/http_mapping_proxy_conf_generator.rb
@@ -5,39 +5,57 @@ class HttpMappingProxyConfGenerator
   # Parameterized by cloud site ID
 
   def run(compute_site_id)
-
-    begin
-      cs = ComputeSite.find(compute_site_id)
-    rescue ActiveRecord::RecordNotFound
-      raise Air::UnknownComputeSite.new "Compute site with id #{compute_site_id.to_s} is unknown."
-    end
-
-    proxy_configuration = []
-    appliances = Appliance.joins(:virtual_machines).where(virtual_machines: {compute_site: cs})
-
-    appliances.each do |appliance|
-      ips = appliance.virtual_machines.select(:ip).collect {|vm| vm.ip}
-      pm_templates = appliance.development? ? appliance.dev_mode_property_set.port_mapping_templates : appliance.appliance_type.port_mapping_templates
-      pm_templates.each do |pmt|
-        proxy_configuration << generate_redirection_and_port_mapping(appliance, pmt, ips, :http) if pmt.http?
-        proxy_configuration << generate_redirection_and_port_mapping(appliance, pmt, ips, :https) if pmt.https?
-      end
-    end
-
-    proxy_configuration.uniq
+    appliances = compute_site_appliances(compute_site(compute_site_id))
+    appliances.collect do |appliance|
+      appliance_proxy_configuration(appliance)
+    end.flatten.uniq
   end
 
   private
 
+  def compute_site_appliances(cs)
+    Appliance.joins(:virtual_machines).where(virtual_machines: {compute_site: cs})
+  end
+
+  def compute_site(compute_site_id)
+    begin
+      ComputeSite.find(compute_site_id)
+    rescue ActiveRecord::RecordNotFound
+      raise Air::UnknownComputeSite.new "Compute site with id #{compute_site_id.to_s} is unknown."
+    end
+  end
+
+  def appliance_proxy_configuration(appliance)
+    ips = ips(appliance)
+    appl_proxy_configuration = []
+    pm_templates(appliance).each do |pmt|
+      appl_proxy_configuration << generate_redirection_and_port_mapping(appliance, pmt, ips, :http) if pmt.http?
+      appl_proxy_configuration << generate_redirection_and_port_mapping(appliance, pmt, ips, :https) if pmt.https?
+    end
+
+    appl_proxy_configuration
+  end
+
   def generate_redirection_and_port_mapping(appliance, pmt, ips, type)
     redirection = redirection(appliance, pmt, ips, type)
+    get_or_create_port_mapping(appliance, pmt, type, redirection[:path])
+    redirection
+  end
+
+  def get_or_create_port_mapping(appliance, pmt, type, path)
     pm = appliance.http_mappings.find_or_create_by(port_mapping_template: pmt, application_protocol: type)
-    pm.url = redirection[:path]
+    pm.url = path
     unless pm.save
       logger.error "Unable to save port mapping for #{appliance.id} appliance, #{pmt.id} port mapping because of #{pm.errors.to_json}"
     end
+  end
 
-    redirection
+  def ips(appliance)
+    appliance.virtual_machines.select(:ip).collect {|vm| vm.ip}
+  end
+
+  def pm_templates(appliance)
+    appliance.development? ? appliance.dev_mode_property_set.port_mapping_templates : appliance.appliance_type.port_mapping_templates
   end
 
   def redirection(appliance, pmt, ips, type)
