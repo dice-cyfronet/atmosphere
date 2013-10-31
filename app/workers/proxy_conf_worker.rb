410289ff76900b1602e1174baf4afe88b4473bc2
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-10-31 15:36:28 +0100

    proxy conf worker triggered when compute site has regenerate_proxy_conf flag set to true

--- app/workers/proxy_conf_worker.rb
+++ app/workers/proxy_conf_worker.rb
@@ -1,4 +1,7 @@
 class ProxyConfWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :proxyconf
 
   def perform(compute_site_id)
     begin
@@ -6,8 +9,19 @@ class ProxyConfWorker
       generator = SiteProxyConf.new(compute_site)
 
       Sidekiq::Client.push('queue' => compute_site.site_id, 'class' => Redirus::Worker::Proxy, 'args' => [generator.generate, generator.properties])
+      compute_site.update(regenerate_proxy_conf: false)
     rescue ActiveRecord::RecordNotFound
       Rails.logger.error "Compute site with id #{compute_site_id.to_s} is unknown. Proxy conf not generated."
+    end
+  end
+
+  def self.regeneration_required(compute_site)
+    compute_site.update(regenerate_proxy_conf: true)
+  end
+
+  def self.regenerate_proxy_confs
+    ComputeSite.where(regenerate_proxy_conf: true).each do |cs|
+      perform_async(cs.id)
     end
   end
 end
