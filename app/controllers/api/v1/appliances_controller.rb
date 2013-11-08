1bc796654655903cf41147b061e634b730908e15
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-11-08 08:55:28 +0100

    appliance controller code simplified after introducing flat rest api

--- app/controllers/api/v1/appliances_controller.rb
+++ app/controllers/api/v1/appliances_controller.rb
@@ -1,8 +1,6 @@
 module Api
   module V1
     class AppliancesController < Api::ApplicationController
-      before_filter :unify_appliance_set_id, only: :create
-      load_resource :appliance_set, only: :create
       before_filter :create_appliance, only: :create
       load_and_authorize_resource :appliance
       before_filter :check_for_conflict!, only: :create
@@ -17,7 +15,7 @@ module Api
       end
 
       def create
-        @appliance_set.transaction do
+        @appliance.transaction do
           @appliance.appliance_type = config_template.appliance_type
 
           raise CanCan::AccessDenied if cannot_create_appliance?
@@ -49,22 +47,22 @@ module Api
         end
       end
 
-      def unify_appliance_set_id
-        params[:appliance_set_id] ||= params[:appliance][:appliance_set_id] if params[:appliance]
+      def create_appliance
+        @appliance = Appliance.new(create_params)
       end
 
-      def create_appliance
-        @appliance = @appliance_set.appliances.create
+      def create_params
+        params.require(:appliance).permit(:appliance_set_id)
       end
 
       def check_for_conflict!
-        if @appliance_set.production? and not appliance_unique?
+        if @appliance.appliance_set.production? and not appliance_unique?
           raise Air::Conflict.new 'You are not allowed to start 2 appliances with the same type and configuration in production appliance set'
         end
       end
 
       def appliance_unique?
-        Appliance.joins(:appliance_configuration_instance).where(appliance_configuration_instances: {payload: configuration_instance.payload}, appliance_set: @appliance_set, appliance_type: config_template.appliance_type).count == 0
+        Appliance.joins(:appliance_configuration_instance).where(appliance_configuration_instances: {payload: configuration_instance.payload}, appliance_set: @appliance.appliance_set, appliance_type: config_template.appliance_type).count == 0
       end
 
       def configuration_instance
@@ -80,7 +78,7 @@ module Api
       end
 
       def config_template_id
-        params[:appliance][:configuration_template_id] if params[:appliance]
+        params[:appliance][:configuration_template_id]
       end
 
       def config_params
