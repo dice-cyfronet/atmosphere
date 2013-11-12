7d2fab7a630f21e222392e96a7319b938c1b388f
gubala <t.gubala@cyfronet.pl>
2013-11-12 20:02:02 +0100

    PMT serializer - added _id to the relation names

--- app/serializers/port_mapping_template_serializer.rb
+++ app/serializers/port_mapping_template_serializer.rb
@@ -3,6 +3,6 @@ class PortMappingTemplateSerializer < ActiveModel::Serializer
 
   attributes :id, :transport_protocol, :application_protocol, :service_name, :target_port
 
-  has_one :appliance_type, key: :appliance_type
-  has_one :dev_mode_property_set, key: :dev_mode_property_set
+  has_one :appliance_type
+  has_one :dev_mode_property_set
 end
