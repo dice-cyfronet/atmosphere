a8dc8e359e01c2fc7c6a606e9acc4d58fff8a05f
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-11-26 14:34:28 +0100

    #37 allow to update proxy url

--- app/controllers/admin/compute_sites_controller.rb
+++ app/controllers/admin/compute_sites_controller.rb
@@ -44,7 +44,7 @@ class Admin::ComputeSitesController < ApplicationController
   private
     # Only allow a trusted parameter "white list" through.
     def compute_site_params
-      params.require(:compute_site).permit(:site_id, :name, :location, :site_type, :technology, :config)
+      params.require(:compute_site).permit(:site_id, :name, :location, :site_type, :technology, :config, :http_proxy_url, :https_proxy_url)
     end
 
 end
