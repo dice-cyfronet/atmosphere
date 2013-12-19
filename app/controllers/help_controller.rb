5707257f67e08fb6a5ff2aedaa0ed9e9b747c15e
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-12-19 10:51:32 +0100

    user need to be looged in to access help page

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,13 +1,13 @@
 class HelpController < ApplicationController
   skip_authorization_check
 
+  before_filter :authenticate_user!
+
   def index
 
   end
 
   def api
-    authenticate_user!
-
     @category = params[:category]
     @category = "README" if @category.blank?
 
