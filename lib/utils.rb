3c85d9eac59f539519705d072cf1d9b6a0a51ea4
tomek <tomek.bartynski@gmail.com>
2014-01-22 19:05:36 +0100

    added a method that returns an array with max values from input array

--- lib/utils.rb
+++ lib/utils.rb
@@ -20,4 +20,24 @@ module Utils
     min_elements.compact
   end
 
+  def max_elements_by(arr)
+    max_elements = [arr.shift]
+    arr.each do |e|
+      if block_given?
+        if yield(max_elements.first) < yield(e)
+          max_elements = [e]
+        elsif yield(max_elements.first) == yield(e)
+          max_elements << e
+        end
+      else
+        if max_elements.first < e
+          max_elements = [e]
+        elsif max_elements.first == e
+          max_elements << e
+        end
+      end
+    end
+    max_elements.compact
+  end
+
 end
