51c5cd42f0d26915bd53f7df7abdd5bc09c8e70e
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-02-21 12:40:04 +0100

    Ref #2321: vm destroyer
    VmDestroyer is responsible for destroying VM and notifying
    connected appliances about this situation.
    
    Right now it passes 'delete_in_cloud' parameter into VM. In the feature
    I propose to use composition here and create something like CloudVmDestroyer,
    which at the beginning invoke VmDestroyer#destroy and than remove VM from
    cloud.

---
+++ app/services/vm_destroyer.rb
@@ -0,0 +1,23 @@
+class VmDestroyer
+  delegate :errors, to: :virtual_machine
+
+  def initialize(virtual_machine, appl_updater_class = ApplianceProxyUpdater)
+    @virtual_machine = virtual_machine
+    @appl_updater_class = appl_updater_class
+  end
+
+  def destroy(delete_in_cloud = true)
+    # to_a because we need to be sure appliances are
+    # loaded before VM is destroyed.
+    affected_appliances = virtual_machine.appliances.to_a
+    virtual_machine.destroy(delete_in_cloud).tap do |destroyed|
+      affected_appliances.each do |appl|
+        appl_updater_class.new(appl).update
+      end
+    end
+  end
+
+  private
+
+  attr_reader :virtual_machine, :appl_updater_class
+end
