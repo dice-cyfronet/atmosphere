e8aced349cf26987aa5c2953277fc31594f972dc
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-02-26 08:00:49 +0100

    Ref #2321: appliance updated on PMT create/update/destroy API action

--- app/controllers/api/v1/port_mapping_templates_controller.rb
+++ app/controllers/api/v1/port_mapping_templates_controller.rb
@@ -5,6 +5,7 @@ module Api
       before_filter :find_port_mapping_templates, only: :index
       load_and_authorize_resource :port_mapping_template, except: :index
       authorize_resource :port_mapping_template, only: :index
+      before_filter :initialize_manager, only: [:create, :update, :destroy]
       respond_to :json
 
       def index
@@ -17,26 +18,26 @@ module Api
 
       def create
         log_user_action "create new port mapping template with following params #{params}"
-        @port_mapping_template.save!
-        render json: @port_mapping_template, status: :created
+        @manager.save!
+        render json: @manager.port_mapping_template, status: :created
         log_user_action "port mapping template created: #{@port_mapping_template.to_json}"
       end
 
       def update
         log_user_action "update port mapping template #{@port_mapping_template.id} with following params #{params}"
         update_params = port_mapping_template_params
-        @port_mapping_template.update_attributes!(update_params)
-        render json: @port_mapping_template
+        @manager.update!(update_params)
+        render json: @manager.port_mapping_template
         log_user_action "port mapping template updated: #{@port_mapping_template.to_json}"
       end
 
       def destroy
         log_user_action "destroy port mapping template #{@port_mapping_template.id}"
-        if @port_mapping_template.destroy
+        if @manager.destroy
           render json: {}
           log_user_action "port mapping template destroyed: #{@port_mapping_template.id}"
         else
-          render_error @port_mapping_template
+          render_error @manager.port_mapping_template
         end
       end
 
@@ -59,6 +60,9 @@ module Api
             :service_name, :target_port, :transport_protocol, :application_protocol, :appliance_type_id, :dev_mode_property_set_id)
       end
 
+      def initialize_manager
+        @manager = PmtManager.new(@port_mapping_template)
+      end
     end # of PortMappingTemplatesController
   end # of V1
 end # of Api
--- app/services/pmt_manager.rb
+++ app/services/pmt_manager.rb
@@ -1,32 +1,34 @@
 class PmtManager
 
-  def initialize(pmt, appl_updater_class=ApplianceProxyUpdater)
-    @pmt = pmt
+  attr_reader :port_mapping_template
+
+  def initialize(port_mapping_template, appl_updater_class=ApplianceProxyUpdater)
+    @port_mapping_template = port_mapping_template
     @appl_updater_class = appl_updater_class
   end
 
   def save!
-    pmt.save!
+    port_mapping_template.save!
     update_affected_appliances
   end
 
   def destroy
-    affected_appliances = Appliance.with_pmt(pmt).to_a
-    pmt.destroy.tap do |destroyed|
+    affected_appliances = Appliance.with_pmt(port_mapping_template).to_a
+    port_mapping_template.destroy.tap do |destroyed|
       update_affected_appliances(affected_appliances) if destroyed
     end
   end
 
   def update!(update_params)
-    pmt.update_attributes!(update_params)
+    port_mapping_template.update_attributes!(update_params)
     update_affected_appliances
   end
 
   private
 
-  attr_reader :pmt, :appl_updater_class
+  attr_reader :appl_updater_class
 
-  def update_affected_appliances(appl=Appliance.with_pmt(pmt))
+  def update_affected_appliances(appl=Appliance.with_pmt(port_mapping_template))
     appl.each { |appl| appl_updater_class.new(appl).update }
   end
 end
