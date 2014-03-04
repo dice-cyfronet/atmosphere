8f6c3109cdd5f299c324d74ff64d96404589a70d
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-03-04 14:33:36 +0100

    Ref #2321: http_mappings#compute_site_id can be null since http_mappings already exists

--- db/migrate/20140218074453_add_compute_site_to_http_mapping.rb
+++ db/migrate/20140218074453_add_compute_site_to_http_mapping.rb
@@ -1,7 +1,7 @@
 class AddComputeSiteToHttpMapping < ActiveRecord::Migration
   def change
     change_table :http_mappings do |t|
-      t.references :compute_site, null: false
+      t.references :compute_site, null: true
     end
 
     add_foreign_key :http_mappings, :compute_sites
