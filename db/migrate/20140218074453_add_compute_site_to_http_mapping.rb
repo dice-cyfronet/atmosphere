af0d680b07aa589be6dcd04aed035a13962c5e78
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-03-04 15:07:06 +0100

    Ref #2321: migration updates compute site and appliance http mappings

--- db/migrate/20140218074453_add_compute_site_to_http_mapping.rb
+++ db/migrate/20140218074453_add_compute_site_to_http_mapping.rb
@@ -4,6 +4,19 @@ class AddComputeSiteToHttpMapping < ActiveRecord::Migration
       t.references :compute_site, null: true
     end
 
+    puts "Setting compute site for existing http mappings"
+    HttpMapping.all.each do |mapping|
+      unless mapping.compute_site.blank?
+        mapping.compute_site = mapping.appliance.virtual_machines.first.compute_site
+        mapping.save!
+      end
+    end
+
+    puts "Updating http mappings for all existing appliances"
+    Appliance.all.each do |appl|
+      ApplianceProxyUpdater.new(appl).update
+    end
+
     add_foreign_key :http_mappings, :compute_sites
   end
 end
