d2186c718cec13fdf55269cc842b67ffdeefa790
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-03-05 12:38:11 +0100

    Ref #2321: add compute site into http mapping migration improved

--- db/migrate/20140218074453_add_compute_site_to_http_mapping.rb
+++ db/migrate/20140218074453_add_compute_site_to_http_mapping.rb
@@ -1,22 +1,21 @@
 class AddComputeSiteToHttpMapping < ActiveRecord::Migration
-  def change
-    change_table :http_mappings do |t|
-      t.references :compute_site, null: true
-    end
+  def up
+    puts "Deleting all old http mappings"
+    HttpMapping.delete_all
 
-    puts "Setting compute site for existing http mappings"
-    HttpMapping.all.each do |mapping|
-      unless mapping.compute_site.blank?
-        mapping.compute_site = mapping.appliance.virtual_machines.first.compute_site
-        mapping.save!
-      end
+    change_table :http_mappings do |t|
+      t.references :compute_site, null: false
     end
+    add_foreign_key :http_mappings, :compute_sites
 
     puts "Updating http mappings for all existing appliances"
     Appliance.all.each do |appl|
       ApplianceProxyUpdater.new(appl).update
     end
+  end
 
-    add_foreign_key :http_mappings, :compute_sites
+  def down
+    remove_foreign_key :http_mappings, :compute_sites
+    remove_column :http_mappings, :compute_site_id
   end
 end
