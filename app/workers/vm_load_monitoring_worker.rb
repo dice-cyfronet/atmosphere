e5bd6f64f195a52080c1b15c30efdd9f4d195767
tomek <tomek.bartynski@gmail.com>
2014-03-10 14:46:02 +0100

    added tests for load metrics monitoring

---
+++ app/workers/vm_load_monitoring_worker.rb
@@ -0,0 +1,17 @@
+require 'zabbix'
+
+class VmLoadMonitoringWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :monitoring
+  sidekiq_options :retry => false
+
+  def perform
+    VirtualMachine.all.each do |vm|
+      if vm.zabbix_host_id
+        metrics = vm.current_load_metrics 
+        vm.save_load_metrics(metrics)
+      end
+    end
+  end
+end
