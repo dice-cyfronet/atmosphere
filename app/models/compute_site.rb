3af9fc4a0df67eec75112bc15a0fc99362677379
Piotr Nowakowski <ymnowako@cyf-kr.edu.pl>
2014-03-17 14:24:55 +0100

    Funds are now bound to specific ComputeSites and the "can afford" methods in BillingManager take this into account.

--- app/models/compute_site.rb
+++ app/models/compute_site.rb
@@ -34,6 +34,9 @@ class ComputeSite < ActiveRecord::Base
   has_many :port_mapping_properties, dependent: :destroy
   has_many :virtual_machine_flavors, dependent: :destroy
 
+  has_many :funds, through: :compute_site_funds
+  has_many :compute_site_funds, dependent: :destroy
+
   scope :with_appliance_type, ->(appliance_type) { joins(virtual_machines: {appliances: :appliance_set}).where(appliances: {appliance_type_id: appliance_type.id}, appliance_sets: {appliance_set_type: [:workflow, :portal]}).readonly(false) }
 
   scope :with_deployment, ->(deployment) { joins(virtual_machines: :deployments).where(deployments: {id: deployment.id}).readonly(false) }
--- app/models/fund.rb
+++ app/models/fund.rb
@@ -16,12 +16,16 @@
 # Each user may belong to one or more fund and each fund may include multiple users
 # Funds are linked to Appliances instead of VMs due to the fact that a single VM may be shared by multiple Appliances
 # (and therefore multiple users), in which case all owners "chip in" to enable continued operation of the VM.
+# Funds are also linked to ComputeSites and may only be used to pay for VMs which belong to their respective ComputeSites.
 
 class Fund < ActiveRecord::Base
 
   has_many :appliances
   has_many :users, through: :user_funds
   has_many :user_funds, dependent: :destroy
+
+  has_many :compute_sites, through: :compute_site_funds
+  has_many :compute_site_funds, dependent: :destroy
 
   validates_presence_of :name
   validates_numericality_of :balance
