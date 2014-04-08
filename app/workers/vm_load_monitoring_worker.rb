7eab789c6aa9119f942dd5847473c9e23c6f795c
tomek <tomek.bartynski@gmail.com>
2014-04-08 17:13:01 +0200

    added logs in load monitoring worker

--- app/workers/vm_load_monitoring_worker.rb
+++ app/workers/vm_load_monitoring_worker.rb
@@ -7,11 +7,13 @@ class VmLoadMonitoringWorker
   sidekiq_options :retry => false
 
   def perform
+    Rails.logger.info "Started load monitoring at #{Time.now}"
     VirtualMachine.all.each do |vm|
       if vm.managed_by_atmosphere && vm.zabbix_host_id
         metrics = vm.current_load_metrics 
         vm.save_load_metrics(metrics)
       end
     end
+    Rails.logger.info "Finished load monitoring at #{Time.now}"
   end
 end
