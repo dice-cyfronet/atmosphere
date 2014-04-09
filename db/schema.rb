d3e9ecf2d3961ffe39ca74efe5ee97b6cafd3189
tomek <tomek.bartynski@gmail.com>
2014-04-09 22:12:22 +0200

    applied migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140325153430) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -280,27 +276,27 @@ ActiveRecord::Schema.define(version: 20140325153430) do
     t.string   "id_at_site",                               null: false
     t.string   "name",                                     null: false
     t.string   "state",                                    null: false
-    t.boolean  "managed_by_atmosphere", default: false,    null: false
     t.integer  "compute_site_id",                          null: false
     t.integer  "virtual_machine_id"
     t.integer  "appliance_type_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "managed_by_atmosphere"
     t.string   "architecture",          default: "x86_64"
   end
 
   add_index "virtual_machine_templates", ["compute_site_id", "id_at_site"], name: "index_vm_tmpls_on_cs_id_and_id_at_site", unique: true, using: :btree
 
   create_table "virtual_machines", force: true do |t|
-    t.string   "id_at_site",                                  null: false
-    t.string   "name",                                        null: false
-    t.string   "state",                                       null: false
+    t.string   "id_at_site",                  null: false
+    t.string   "name",                        null: false
+    t.string   "state",                       null: false
     t.string   "ip"
-    t.boolean  "managed_by_atmosphere",       default: false, null: false
-    t.integer  "compute_site_id",                             null: false
+    t.integer  "compute_site_id",             null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "virtual_machine_template_id"
+    t.boolean  "managed_by_atmosphere"
     t.integer  "virtual_machine_flavor_id"
     t.integer  "zabbix_host_id"
   end
