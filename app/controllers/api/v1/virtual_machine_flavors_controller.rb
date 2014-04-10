0e617625df42068d2e192f8b37452b04f014319d
tomek <tomek.bartynski@gmail.com>
2014-04-11 00:22:04 +0200

    almost implemented getting flavors with filters (init conf filter pending)

--- app/controllers/api/v1/virtual_machine_flavors_controller.rb
+++ app/controllers/api/v1/virtual_machine_flavors_controller.rb
@@ -2,11 +2,66 @@ module Api
   module V1
 
     class VirtualMachineFlavorsController < Api::ApplicationController
-      load_and_authorize_resource :virtual_machine_flavor
+      authorize_resource :virtual_machine_flavor
       respond_to :json
 
+      # Query params may include either:
+      # - appliance_configuration_instance_id and optionally compute_site_id
+      # - appliance_type_id and optionally compute_site_id
+      # - a combination of compute_site_id, cpu, memory, hdd
+      # or be empty.
       def index
+        #check_filters!
+        @virtual_machine_flavors = []
+        if filters_empty? ||requirements_filters?
+          conditions_str = build_condition_str
+          @virtual_machine_flavors = VirtualMachineFlavor.where(conditions_str, params)
+        elsif appliance_conf_inst_filters?
+
+        elsif appliance_type_filters?
+          tmpls = params['compute_site_id'] ? VirtualMachineTemplate.where(appliance_type_id: params['appliance_type_id'], state: 'active', compute_site_id: params['compute_site_id']) : VirtualMachineTemplate.where(appliance_type_id: params['appliance_type_id'], state: 'active')
+          unless tmpls.blank?
+            tmpl, flavor = Optimizer.instance.select_tmpl_and_flavor(tmpls)
+            @virtual_machine_flavors = [flavor]
+          end
+        else
+          raise Air::Conflict.new("Illegal combination of filters")
+        end
         respond_with @virtual_machine_flavors
+      end
+
+      private
+      def build_condition_str
+        cs_condition = params['compute_site_id'] ? "compute_site_id = :compute_site_id" : ''
+        cpu_condition = params['cpu'] ? "cpu >= :cpu" : ''
+        mem_condition = params['memory'] ? "memory >= :memory" : ''
+        hdd_condition = params['hdd'] ? "hdd >= :hdd" : ''
+        [cs_condition, cpu_condition, mem_condition, hdd_condition].reject{|e| e.blank?}.join(" AND ")
+      end
+
+      
+      def check_filters!
+        return if (filters_empty? || appliance_conf_inst_filters? || appliance_type_filters? || requirements_filters?)
+        raise Air::Conflict.new("Illegal combination of filters")
+      end
+
+      def filters_empty?
+        (params.keys & ['appliance_configuration_instance_id', 'appliance_type_id','compute_site_id', 'cpu', 'memory', 'hdd']).empty?
+      end
+
+      def appliance_conf_inst_filters?
+        keys = params.keys
+        (keys.include? 'appliance_configuration_instance_id') && (keys & ['appliance_type_id', 'cpu', 'memory', 'hdd']).empty?
+      end
+
+      def appliance_type_filters?
+        keys = params.keys
+        (keys.include? 'appliance_type_id') && (keys & ['appliance_configuration_instance_id', 'cpu', 'memory', 'hdd']).empty?
+      end
+
+      def requirements_filters?
+        keys = params.keys
+        (not (keys & ['compute_site_id', 'cpu', 'memory', 'hdd']).empty?) && (keys & ['appliance_configuration_instance_id', 'appliance_type_id']).empty?
       end
 
     end
--- spec/requests/api/virtual_machine_flavors_spec.rb
+++ spec/requests/api/virtual_machine_flavors_spec.rb
@@ -18,8 +18,15 @@ describe Api::V1::VirtualMachineFlavorsController do
 
     context 'when authenticated' do
 
+      required_mem = 1024
+      let(:cs) { create(:compute_site) }
+      let(:cs2) { create(:compute_site) }
       let!(:f1) { create(:virtual_machine_flavor) }
       let!(:f2) { create(:virtual_machine_flavor) }
+      let!(:f3) { create(:virtual_machine_flavor, compute_site: cs) }
+      let!(:f4) { create(:virtual_machine_flavor, memory: required_mem - 256) }
+      let!(:f5) { create(:virtual_machine_flavor, memory: required_mem) }
+      let!(:f6) { create(:virtual_machine_flavor, memory: required_mem + 256, compute_site: cs2) }
 
       it 'returns 200' do
         get api('/virtual_machine_flavors', user)
@@ -29,11 +36,95 @@ describe Api::V1::VirtualMachineFlavorsController do
       it 'returns all flavors when no filters are specified' do
         get api('/virtual_machine_flavors', user)
         flavors = fls_response
-        expect(flavors.size).to eq 2
+        expect(flavors.size).to eq VirtualMachineFlavor.count
       end
 
-      context 'when filter' do
+      context 'filters validation' do
+        it "returns 409 conflict for conflicting filters" do
+          get api('/virtual_machine_flavors?appliance_configuration_instance_id=1&appliance_type_id=1', user)
+          expect(response.status).to eq 409
+        end
 
+        it "returns 200 for empty filters" do
+          get api('/virtual_machine_flavors', user)
+          expect(response.status).to eq 200
+        end
+      end
+
+      context 'when filter defined for compute site' do
+        
+        it "returns flavors at given cs only" do
+          get api("/virtual_machine_flavors?compute_site_id=#{cs.id}", user)
+          flavors = fls_response
+          expect(flavors.size).to eq 1
+          expect(flavors.first['compute_site_id']).to eq cs.id
+        end
+      end
+
+      context 'when requirements filter defined' do
+
+        it "returns flavors with memory grater or equal to specified" do          
+          get api("/virtual_machine_flavors?memory=#{required_mem}", user)
+          flavors = fls_response
+          expect(flavors.size).to be >= 2
+          flavors.each{|f| expect(f['memory']).to be >= required_mem }
+        end
+
+        it "returns flavors with memory grater or equal to specified and available at given cs" do
+          get api("/virtual_machine_flavors?memory=#{required_mem}&compute_site_id=#{cs2.id}", user)
+          flavors = fls_response
+          expect(flavors.size).to be >= 1
+          flavors.each{|f|
+            expect(f['memory']).to be >= required_mem
+            expect(f['compute_site_id']).to eq cs2.id
+          }
+        end
+      end
+
+      context 'filter for appliance type defined' do
+        let(:at) { create(:appliance_type, preference_memory:required_mem) }
+
+        it "returns empty list if AT does not exist" do
+          get api("/virtual_machine_flavors?appliance_type_id=#{at.id + 1}", user)
+          flavors = fls_response
+          expect(flavors.size).to eq 0
+        end
+
+        it "returns empty list if there are no templates for AT" do
+          get api("/virtual_machine_flavors?appliance_type_id=#{at.id}", user)
+          flavors = fls_response
+          expect(flavors.size).to eq 0
+        end
+
+        it "returns one flavor with memory greater or equal to preference memory specified in at" do
+          tmpl = create(:virtual_machine_template, appliance_type: at, compute_site: cs2, state: 'active')
+          get api("/virtual_machine_flavors?appliance_type_id=#{at.id}", user)
+          flavors = fls_response
+          expect(flavors.size).to eq 1
+          fl = flavors.first
+          expect(fl['memory']).to be >= required_mem
+          expect(fl['compute_site_id']).to eq tmpl.compute_site_id
+        end
+
+        context 'filter for cs specified' do
+          it "applies compute site filtering" do
+            tmpl = create(:virtual_machine_template, appliance_type: at, compute_site: cs2, state: 'active')
+            get api("/virtual_machine_flavors?appliance_type_id=#{at.id}&compute_site_id=#{cs2.id + 1}", user)
+            flavors = fls_response
+            expect(flavors.size).to eq 0
+          end
+
+        end
+
+      end
+
+      context 'filter for appliance configuration instance specified' do
+        context 'vm can be reused' do
+        end
+
+        context 'vm cannot be reused' do
+
+        end
       end
 
     end
