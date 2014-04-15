ec82cdf2532b10db47be6de1c24003825c158b01
gubala <tg.errors@gmail.com>
2014-04-15 18:06:06 +0200

    added in-place confirmation to appliance type delete

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140414112009) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "appliance_compute_sites", force: true do |t|
     t.integer "appliance_id"
     t.integer "compute_site_id"
