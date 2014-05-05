afab03ec8fdccd8121be7c3af98a72cb17e70953
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-05-05 15:16:24 +0200

    Ref #2905: check if vm can be assigned to appliance, notify when not

--- app/services/appliance_vms_manager.rb
+++ app/services/appliance_vms_manager.rb
@@ -1,8 +1,10 @@
+#
+# Manager for starting and reusing VM for specified appliance.
+#
 class ApplianceVmsManager
-
   def initialize(appliance,
-      updater_class=Proxy::ApplianceProxyUpdater,
-      vm_creator_class=Cloud::VmCreator)
+      updater_class = Proxy::ApplianceProxyUpdater,
+      vm_creator_class = Cloud::VmCreator)
     @appliance = appliance
     @updater = updater_class.new(appliance)
     @vm_creator_class = vm_creator_class
@@ -13,12 +15,19 @@ class ApplianceVmsManager
   end
 
   def reuse_vm!(vm)
-    BillingService.can_afford_vm?(appliance, vm) ?
-      add_vm(vm) : not_enough_funds
+    if BillingService.can_afford_vm?(appliance, vm)
+      add_vm(vm)
+    else
+      not_enough_funds
+    end
   end
 
   def spawn_vm!(tmpl, flavor, name)
-    BillingService.can_afford_flavor?(appliance, flavor) ? instantiate_vm(tmpl, flavor, name) : not_enough_funds
+    if BillingService.can_afford_flavor?(appliance, flavor)
+      instantiate_vm(tmpl, flavor, name)
+    else
+      not_enough_funds
+    end
   end
 
   def save
@@ -30,19 +39,43 @@ class ApplianceVmsManager
   attr_reader :appliance, :updater
 
   def not_enough_funds
+    unsatisfied('Not enough funds')
+    appliance.billing_state = 'expired'
+  end
+
+  def unsatisfied(msg)
     appliance.state = :unsatisfied
-    appliance.billing_state = "expired"
-    appliance.state_explanation = 'Not enough funds'
+    appliance.state_explanation = msg
   end
 
   def instantiate_vm(tmpl, flavor, name)
     server_id = start_vm_on_cloud(tmpl, flavor, name)
-    vm = appliance.virtual_machines.create(name: name, source_template: tmpl, state: :build, virtual_machine_flavor: flavor, managed_by_atmosphere: true, id_at_site: server_id, compute_site: tmpl.compute_site)
-    appliance_satisfied(vm)
+    vm = appliance.virtual_machines.create(
+        name: name, source_template: tmpl,
+        state: :build, virtual_machine_flavor: flavor,
+        managed_by_atmosphere: true, id_at_site: server_id,
+        compute_site: tmpl.compute_site
+      )
+
+    if vm.valid?
+      appliance_satisfied(vm)
+    else
+      unsatisfied('Unable to assign VM to appliance - please contact administrator')
+      Raven.capture_message('Unable to assign VM to appliance',
+          logger: 'error',
+          extra: {
+            is_at_site: server_id,
+            appliance_name: appliance.name,
+            appliance_id: appliance.id,
+            errors: vm.errors.to_json
+          }
+        )
+    end
   end
 
   def start_vm_on_cloud(tmpl, flavor, name)
-    @vm_creator_class.new(tmpl,
+    @vm_creator_class.new(
+        tmpl,
         flavor: flavor, name: name,
         user_data: appliance.user_data,
         user_key: appliance.user_key
@@ -60,6 +93,11 @@ class ApplianceVmsManager
   end
 
   def bill
-    BillingService.bill_appliance(appliance, Time.now.utc, "Optimization completed - performing billing action.", true) if appliance.state.satisfied?
+    BillingService.bill_appliance(
+        appliance,
+        Time.now.utc,
+        'Optimization completed - performing billing action.',
+        true
+      ) if appliance.state.satisfied?
   end
 end
