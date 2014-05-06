81e198acb4a853953c9471e8c33742979015e9c3
Bartosz Wilk <b.wilk@cyfronet.pl>
2014-05-06 10:54:39 +0200

    Added tests for key validation

--- spec/models/user_key_spec.rb
+++ spec/models/user_key_spec.rb
@@ -41,10 +41,6 @@ describe UserKey do
     end
   end
 
-  it 'should import key to cloud site', :focus=>true do
-    ComputeSite.all.each { |cs| subject.import_to_cloud(cs) }
-  end
-
   it 'should not raise error if key is imported twice' do
     subject.name
     ComputeSite.all.each { |cs| subject.import_to_cloud(cs); subject.import_to_cloud(cs) }
@@ -94,4 +90,19 @@ describe UserKey do
       expect(key.id_at_site).to start_with('john-')
     end
   end
+
+  describe 'key validation', :focus=>true do
+
+    it 'should pass on valid ssh-rsa key' do
+      create(:user_key, public_key: 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAklOUpkDHrfHY17SbrmTIpNLTGK9Tjom/BWDSUGPl+nafzlHDTYW7hdI4yZ5ew18JH4JW9jbhUFrviQzM7xlELEVf4h9lFX5QVkbPppSwg0cda3Pbv7kOdJ/MTyBlWXFCR+HAo3FXRitBqxiX1nKhXpHAZsMciLq8V6RjsNAQwdsdMFvSlVK/7XAt3FaoJoAsncM1Q9x5+3V0Ww68/eIFmb1zuUFljQJKprrX88XypNDvjYNby6vw/Pb0rwert/EnmZ+AW4OZPnTPI89ZPmVMLuayrD2cE86Z/il8b+gw3r3+1nKatmIkjn2so1d01QraTlMqVSsbxNrRFi9wrf+M7Q== factorized@sting')
+    end
+
+    it 'should fail on ssh-dsa key' do
+      expect { create(:user_key, public_key: 'ssh-dsa AAAAB3NzaC1yc2EAAAABIwAAAQEAklOUpkDHrfHY17SbrmTIpNLTGK9Tjom/BWDSUGPl+nafzlHDTYW7hdI4yZ5ew18JH4JW9jbhUFrviQzM7xlELEVf4h9lFX5QVkbPppSwg0cda3Pbv7kOdJ/MTyBlWXFCR+HAo3FXRitBqxiX1nKhXpHAZsMciLq8V6RjsNAQwdsdMFvSlVK/7XAt3FaoJoAsncM1Q9x5+3V0Ww68/eIFmb1zuUFljQJKprrX88XypNDvjYNby6vw/Pb0rwert/EnmZ+AW4OZPnTPI89ZPmVMLuayrD2cE86Z/il8b+gw3r3+1nKatmIkjn2so1d01QraTlMqVSsbxNrRFi9wrf+M7Q== factorized@sting') }
+        .to raise_error
+    end
+
+  end
+
+
 end
