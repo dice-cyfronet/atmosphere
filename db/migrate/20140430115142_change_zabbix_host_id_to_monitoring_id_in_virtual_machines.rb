ed58af20fa320771b712d8903fb31b53da9ca435
tomek <tomek.bartynski@gmail.com>
2014-05-19 11:13:22 +0200

    added down method to migration that renames zabbix_host_id to monitoring_id

--- db/migrate/20140430115142_change_zabbix_host_id_to_monitoring_id_in_virtual_machines.rb
+++ db/migrate/20140430115142_change_zabbix_host_id_to_monitoring_id_in_virtual_machines.rb
@@ -1,9 +1,14 @@
 class ChangeZabbixHostIdToMonitoringIdInVirtualMachines < ActiveRecord::Migration
   
-  def change
+  def up
     change_table :virtual_machines do |t|
       t.rename :zabbix_host_id, :monitoring_id
     end
   end
 
+  def down
+    change_table :virtual_machines do |t|
+      t.rename :monitoring_id, :zabbix_host_id
+    end
+  end
 end
