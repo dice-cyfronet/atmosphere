a63147a1afc31ebadf26d6f0110d3a0628004944
tomek <tomek.bartynski@gmail.com>
2014-06-18 11:11:02 +0200

    introduced scope in user for getting users associated with given vm and started using this scope in vm tags mng

--- app/services/cloud/vm_tags_manager.rb
+++ app/services/cloud/vm_tags_manager.rb
@@ -1,7 +1,7 @@
 class Cloud::VmTagsManager
 
   def create_tags_for_vm(vm)
-    users = vm.appliances.collect{|a| a.appliance_set}.collect{|a_set| a_set.user.login}.uniq.join(', ')
+    users = User.with_vm(vm).collect{|u| u.login}.join(', ')
     VmTagsCreatorWorker.perform_async(vm.id_at_site, vm.compute_site.id,
       {'Name' => vm.name, 'Appliance type name' => vm.appliance_type.name, 'Users' => users}
     )
--- spec/services/cloud/vm_tags_manager_spec.rb
+++ spec/services/cloud/vm_tags_manager_spec.rb
@@ -1,4 +1,5 @@
-require 'spec_helper'
+#require 'spec_helper'
+require 'rails_helper'
 
 describe Cloud::VmTagsManager do
 
@@ -6,20 +7,18 @@ describe Cloud::VmTagsManager do
     
     server_name = 'SERVER NAME'
     server_id = 'SERVER_ID'
-    cs_id = 1
-    u1 = double('user1', login: 'user1')
-    u2 = double('user2',login: 'user2')
-    at = double('appliance type',name: 'Ubuntu')
-    a_set_1 = double('appliance set 1', user: u1)
-    a_set_2 = double('appliance set 2', user: u2)
-    appl1 = double('first appliance', appliance_type: at, appliance_set: a_set_1)
-    appl2 = double('second appliance', appliance_type: at, appliance_set: a_set_2)
-    cs = double('compute site', id: cs_id)
-    vm = double('virtual machine', appliances: [appl1, appl2], compute_site: cs, id_at_site: server_id,
-        name: server_name, appliance_type: at)
+    u1 = create(:user, login: 'user1')
+    u2 = create(:user,login: 'user2')
+    at = create(:appliance_type)
+    a_set_1 = create(:appliance_set, user: u1)
+    a_set_2 = create(:appliance_set, user: u2)
+    appl1 = create(:appliance, appliance_type: at, appliance_set: a_set_1)
+    appl2 = create(:appliance, appliance_type: at, appliance_set: a_set_2)
+    cs = create(:compute_site)
+    vm = create(:virtual_machine, appliances: [appl1, appl2], compute_site: cs, id_at_site: server_id)
     
     expect(VmTagsCreatorWorker).to receive(:perform_async)
-      .with(server_id, cs_id, {'Name' => vm.name, 'Appliance type name' => appl1.appliance_type.name,
+      .with(server_id, cs.id, {'Name' => vm.name, 'Appliance type name' => appl1.appliance_type.name,
         'Users' => 'user1, user2'})
     subject.create_tags_for_vm(vm)
   end
