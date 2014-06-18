bbfe3a4a6e76cba529ac56d57ea31779d0a4d329
tomek <tomek.bartynski@gmail.com>
2014-06-18 10:13:30 +0200

    enhanced mock creation for tag testing

--- spec/services/cloud/vm_tags_manager_spec.rb
+++ spec/services/cloud/vm_tags_manager_spec.rb
@@ -2,34 +2,22 @@ require 'spec_helper'
 
 describe Cloud::VmTagsManager do
 
-  it 'tags are created for reused vm' do
-    vm = double('virtual machine')
+  it 'tags worker is invoked' do
+    
     server_name = 'SERVER NAME'
     server_id = 'SERVER_ID'
     cs_id = 1
-    u1 = double('user1')
-    expect(u1).to receive(:login).and_return 'user1'
-    u2 = double('user2')
-    expect(u2).to receive(:login).and_return 'user2'
-    at = double('appliance type')
-    allow(at).to receive(:name).and_return('Ubuntu')
-    appl1 = double('first appliance')
-    appl2 = double('second appliance')
-    allow(appl1).to receive(:appliance_type).and_return(at)
-    allow(appl2).to receive(:appliance_type).and_return(at)
-    a_set_1 = double('appliance set 1')
-    a_set_2 = double('appliance set 2')
-    expect(a_set_1).to receive(:user).and_return u1
-    expect(a_set_2).to receive(:user).and_return u2
-    expect(appl1).to receive(:appliance_set).and_return a_set_1
-    expect(appl2).to receive(:appliance_set).and_return a_set_2
-    expect(vm).to receive(:appliances).and_return [appl1, appl2]
-    cs = double('compute site')
-    allow(vm).to receive(:compute_site).and_return cs
-    allow(vm).to receive(:id_at_site).and_return server_id
-    allow(vm).to receive(:name).and_return server_name
-    allow(vm).to receive(:appliance_type).and_return at
-    allow(cs).to receive(:id).and_return cs_id
+    u1 = double('user1', login: 'user1')
+    u2 = double('user2',login: 'user2')
+    at = double('appliance type',name: 'Ubuntu')
+    a_set_1 = double('appliance set 1', user: u1)
+    a_set_2 = double('appliance set 2', user: u2)
+    appl1 = double('first appliance', appliance_type: at, appliance_set: a_set_1)
+    appl2 = double('second appliance', appliance_type: at, appliance_set: a_set_2)
+    cs = double('compute site', id: cs_id)
+    vm = double('virtual machine', appliances: [appl1, appl2], compute_site: cs, id_at_site: server_id,
+        name: server_name, appliance_type: at)
+    
     expect(VmTagsCreatorWorker).to receive(:perform_async)
       .with(server_id, cs_id, {'Name' => vm.name, 'Appliance type name' => appl1.appliance_type.name,
         'Users' => 'user1, user2'})
