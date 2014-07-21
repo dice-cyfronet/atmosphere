9711046e16627d30a0b009267c5fc98277f874d0
Bartosz Wilk <b.wilk@cyfronet.pl>
2014-07-21 12:57:44 +0200

    Moved user_id from sql query to attribute

--- app/serializers/appliance_type_serializer.rb
+++ app/serializers/appliance_type_serializer.rb
@@ -4,14 +4,17 @@
 class ApplianceTypeSerializer < ActiveModel::Serializer
   embed :ids
 
-  attributes :id, :name, :description, :shared, :scalable, :visible_to
+  attributes :id, :name, :description, :shared, :scalable, :visible_to, :author_id
   attributes :preference_cpu, :preference_memory, :preference_disk
   attributes :active
 
-  has_one :author
   has_one :security_proxy
 
   has_many :appliances, :port_mapping_templates, :appliance_configuration_templates, :virtual_machine_templates, :compute_sites
+
+  def author_id
+    object.user_id
+  end
 
   def active
     active_vmts = object.virtual_machine_templates
