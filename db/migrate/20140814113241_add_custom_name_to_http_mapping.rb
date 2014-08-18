73e47ead65115adf3b370cd4e309255a0679a98c
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-08-18 14:43:00 +0200

    Ref #3175: fill http mapping base_url in migration

--- db/migrate/20140814113241_add_custom_name_to_http_mapping.rb
+++ db/migrate/20140814113241_add_custom_name_to_http_mapping.rb
@@ -1,6 +1,16 @@
 class AddCustomNameToHttpMapping < ActiveRecord::Migration
   def change
-   add_column :http_mappings, :custom_name, :string
-   add_column :http_mappings, :base_url, :string, null: false
+    add_column :http_mappings, :custom_name, :string
+    add_column :http_mappings, :base_url, :string
+
+    HttpMapping.all.each do |hm|
+      hm.base_url = hm.application_protocol.http? ?
+        hm.compute_site.http_proxy_url :
+        hm.compute_site.https_proxy_url
+
+      hm.save
+    end
+
+    change_column :http_mappings, :base_url, :string, null: false
   end
 end
