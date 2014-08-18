4cc8521d57679ae43b1f1744fcda51c9d68d38f5
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-08-18 11:29:09 +0200

    let removed

--- spec/requests/api/http_mappings_spec.rb
+++ spec/requests/api/http_mappings_spec.rb
@@ -4,16 +4,6 @@ describe Api::V1::HttpMappingsController do
 
   include ApiHelpers
 
-  let(:user)  { create(:user) }
-
-  let(:as1) { create(:appliance_set, user: user) }
-  let(:appl1) { create(:appliance, appliance_set: as1) }
-  let!(:hm1) { create(:http_mapping, appliance: appl1) }
-
-  let(:as2) { create(:appliance_set, user: user) }
-  let(:appl2) { create(:appliance, appliance_set: as2) }
-  let!(:hm2) { create(:http_mapping, appliance: appl2) }
-
   describe 'GET /http_mappings' do
     context 'when unauthenticated' do
       it 'returns 401 Unauthorized error' do
@@ -21,51 +11,59 @@ describe Api::V1::HttpMappingsController do
         expect(response.status).to eq 401
       end
     end
+
     context 'when authenticated as user' do
       it 'returns 200 on success' do
+        user = create(:user)
+
         get api("/http_mappings", user)
         expect(response.status).to eq 200
       end
       it 'returns mappings' do
+        user = create(:user)
+        hm1 = user_http_mapping(user)
+        hm2 = user_http_mapping(user)
+
         get api('/http_mappings', user)
         expect(hms_response).to be_an Array
         expect(hms_response.size).to eq 2
         expect(hms_response[0]).to http_mapping_eq hm1
         expect(hms_response[1]).to http_mapping_eq hm2
       end
-    end
-    context 'when exist other users mappings' do
-      let(:user_a)  { create(:user) }
-      let(:as_a) { create(:appliance_set, user: user_a) }
-      let(:appl_a) { create(:appliance, appliance_set: as_a) }
-      let!(:hm_a) { create(:http_mapping, appliance: appl_a) }
-      it 'gets only its mappings' do
-        get api('/http_mappings', user_a)
-        expect(hms_response).to be_an Array
-        expect(hms_response.size).to eq 1
-        expect(hms_response[0]).to http_mapping_eq hm_a
-      end
-    end
 
-    context 'when filter params speciffied' do
-      let(:user_a)  { create(:user) }
-      let(:as_a) { create(:appliance_set, user: user_a) }
-      let(:appl_a) { create(:appliance, appliance_set: as_a) }
-      let(:appl_a2) { create(:appliance, appliance_set: as_a) }
-      let!(:hm_a) { create(:http_mapping, appliance: appl_a) }
-      let!(:hm_a2) { create(:http_mapping, appliance: appl_a2) }
-      it 'finds only mapping with requested id' do
-        get api("/http_mappings?id=#{hm_a2.id}", user_a)
-        expect(hms_response).to be_an Array
+      it 'gets only owned mappings' do
+        user_http_mapping(create(:user))
+        second_user = create(:user)
+        hm = user_http_mapping(second_user)
 
-        expect(hms_response.size).to eq 1
-        expect(hms_response[0]).to http_mapping_eq hm_a2
-      end
-      it 'gets only its mappings' do
-        get api("/http_mappings?appliance_id=#{appl_a.id}", user_a)
+        get api('/http_mappings', second_user)
         expect(hms_response).to be_an Array
         expect(hms_response.size).to eq 1
-        expect(hms_response[0]).to http_mapping_eq hm_a
+        expect(hms_response[0]).to http_mapping_eq hm
+      end
+
+      context 'filter params' do
+        it 'finds only mapping with requested id' do
+          user = create(:user)
+          hm1 = user_http_mapping(user)
+          hm2 = user_http_mapping(user)
+
+          get api("/http_mappings?id=#{hm1.id}", user)
+
+          expect(hms_response).to be_an Array
+          expect(hms_response.size).to eq 1
+          expect(hms_response[0]).to http_mapping_eq hm1
+        end
+        it 'gets only its mappings' do
+          user = create(:user)
+          hm1 = user_http_mapping(user)
+          hm2 = user_http_mapping(user)
+
+          get api("/http_mappings?appliance_id=#{hm1.appliance_id}", user)
+          expect(hms_response).to be_an Array
+          expect(hms_response.size).to eq 1
+          expect(hms_response[0]).to http_mapping_eq hm1
+        end
       end
     end
   end
@@ -73,15 +71,21 @@ describe Api::V1::HttpMappingsController do
   describe 'GET /http_mappings/{id}' do
     context 'when unauthenticated' do
       it 'returns 401 Unauthorized error' do
-        get api("/http_mappings/#{hm1.id}")
+        hm = create(:http_mapping)
+
+        get api("/http_mappings/#{hm.id}")
         expect(response.status).to eq 401
       end
     end
     context 'when authenticated as user' do
       it 'returns 200 on success' do
-        get api("/http_mappings/#{hm1.id}", user)
+        user = create(:user)
+        hm = user_http_mapping(user)
+
+        get api("/http_mappings/#{hm.id}", user)
+
         expect(response.status).to eq 200
-        expect(hm_response).to http_mapping_eq hm1
+        expect(hm_response).to http_mapping_eq hm
       end
     end
   end
@@ -89,15 +93,21 @@ describe Api::V1::HttpMappingsController do
   describe 'GET /http_mappings?appliance_id={id}' do
     context 'when query for exisitng appliance' do
       it 'gets the only mapping that fits the query' do
-        get api("/http_mappings?appliance_id=#{hm1.appliance_id}", user)
+        user = create(:user)
+        user_http_mapping(user)
+        hm = user_http_mapping(user)
+
+        get api("/http_mappings?appliance_id=#{hm.appliance_id}", user)
         expect(response.status).to eq 200
         expect(hms_response.size).to eq 1
-        expect(hms_response[0]).to http_mapping_eq hm1
+        expect(hms_response[0]).to http_mapping_eq hm
       end
     end
     context 'when query for non existing appliance' do
       it 'gets empty answer' do
-        get api("/http_mappings?appliance_id=666", user)
+        user = create(:user)
+
+        get api("/http_mappings?appliance_id=-666", user)
         expect(response.status).to eq 200
         expect(hms_response.size).to eq 0
       end
@@ -112,4 +122,10 @@ describe Api::V1::HttpMappingsController do
     json_response['http_mapping']
   end
 
+  def user_http_mapping(user)
+    as = create(:appliance_set, user: user)
+    appl = create(:appliance, appliance_set: as)
+
+    create(:http_mapping, appliance: appl)
+  end
 end
