6b2173207f08f0f4953ca615d8458c34dfe2bcd8
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-09-10 15:11:02 +0200

    missing ability for developer added in clew REST API

--- app/abilities/clew_ability_builder.rb
+++ app/abilities/clew_ability_builder.rb
@@ -1,13 +1,13 @@
 class ClewAbilityBuilder < AbilityBuilder
 
   def add_user_abilities!
-
     can :appliance_instances, ApplianceSet, user_id: user.id
 
     can :appliance_types, ApplianceType, visible_to: 'all'
     can :appliance_types, ApplianceType, user_id: user.id
-
   end
 
-
+  def add_developer_abilities!
+    can :appliance_types, ApplianceType, visible_to: 'developer'
+  end
 end
