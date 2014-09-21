4c29eb3240d916200fae95865976395a5bf06921
tomek <t.bartynski@cyfronet.pl>
2014-09-21 15:04:36 +0200

    added manual optimization strategy and tests

---
+++ app/providers/optimization_strategy/manual.rb
@@ -0,0 +1,25 @@
+module OptimizationStrategy
+  class Manual < OptimizationStrategy::Default
+    def initialize(appliance)
+      @appliance = appliance
+    end
+
+    def can_reuse_vm?
+      false
+    end
+
+    def vm_to_reuse
+      nil
+    end
+
+    def new_vms_tmpls_and_flavors
+      tmpls_and_flavors = []
+      tmpls = vmt_candidates_for(@appliance)
+      @appliance.optimization_policy_params['vms'].each do |vm|
+        options = {cpu: vm['cpu'], memory: vm['mem']}
+        tmpls_and_flavors += Default.select_tmpls_and_flavors(tmpls, options)
+      end
+      tmpls_and_flavors
+    end
+  end
+end
