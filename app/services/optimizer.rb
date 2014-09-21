d340e1bd59aabbe958452a99e5da2324b57940a9
tomek <t.bartynski@cyfronet.pl>
2014-09-21 15:05:09 +0200

    added support for selecting opt strategy and implementing its decisions

--- app/services/optimizer.rb
+++ app/services/optimizer.rb
@@ -18,32 +18,35 @@ class Optimizer
 
     if appliance.virtual_machines.blank?
       vm_to_be_reused = nil
-      if appl_manager.can_reuse_vm? && !(vm_to_be_reused = optimization_strategy.vm_to_reuse).nil?
+      if optimization_strategy.can_reuse_vm? && !(vm_to_be_reused = optimization_strategy.vm_to_reuse).nil?
         logger.info { " - Reusing #{vm_to_be_reused.id_at_site} for #{appliance.id}" }
         appl_manager.reuse_vm!(vm_to_be_reused)
         logger.info { " - #{vm_to_be_reused.id_at_site} vm reused for #{appliance.id}" }
       else
-        tmpl, flavor = optimization_strategy.new_vm_tmpl_and_flavor
-        if tmpl.blank?
-          appliance.state = :unsatisfied
-          err_msg = "No matching template was found for appliance #{appliance.name}"
-          appliance.state_explanation = err_msg
-          appliance.save
-          logger.warn {
-            " - No matching template was found for appliance with #{appliance.id} id"
-          }
-        elsif flavor.nil?
+        optimization_strategy.new_vms_tmpls_and_flavors.each do |tmpl_and_flavor|
+          tmpl = tmpl_and_flavor[:template]
+          flavor = tmpl_and_flavor[:flavor]
+          if tmpl.blank?
             appliance.state = :unsatisfied
-            err_msg = "No matching flavor was found for appliance #{appliance.name}"
+            err_msg = "No matching template was found for appliance #{appliance.name}"
             appliance.state_explanation = err_msg
+            appliance.save
             logger.warn {
-              " - No matching flavor was found for appliance with #{appliance.id} id"
+              " - No matching template was found for appliance with #{appliance.id} id"
             }
-        else
-          vm_name = appliance.name.blank? ? appliance.appliance_type.name : appliance.name
-          logger.info { " - Spawning new VM from #{tmpl.id_at_site} with #{flavor.id_at_site} flavor for appliance with #{appliance.id} id" }
-          appl_manager.spawn_vm!(tmpl, flavor, vm_name)
-          logger.info { " - New VM from #{tmpl.id_at_site} with #{flavor.id_at_site} flavor started for appliance with #{appliance.id} id" }
+          elsif flavor.nil?
+              appliance.state = :unsatisfied
+              err_msg = "No matching flavor was found for appliance #{appliance.name}"
+              appliance.state_explanation = err_msg
+              logger.warn {
+                " - No matching flavor was found for appliance with #{appliance.id} id"
+              }
+          else
+            vm_name = appliance.name.blank? ? appliance.appliance_type.name : appliance.name
+            logger.info { " - Spawning new VM from #{tmpl.id_at_site} with #{flavor.id_at_site} flavor for appliance with #{appliance.id} id" }
+            appl_manager.spawn_vm!(tmpl, flavor, vm_name)
+            logger.info { " - New VM from #{tmpl.id_at_site} with #{flavor.id_at_site} flavor started for appliance with #{appliance.id} id" }
+          end
         end
       end
       unless appl_manager.save
@@ -64,13 +67,17 @@ class Optimizer
   end
 
   def select_tmpl_and_flavor(tmpls, options={})
-    OptimizationStrategy::Default.select_tmpl_and_flavor(tmpls, options)
+    tmpl_and_flavor = select_tmpls_and_flavors(tmpls, options).first
+    [tmpl_and_flavor[:template], tmpl_and_flavor[:flavor]]
+  end
+
+  def select_tmpls_and_flavors(tmpls, options={})
+    OptimizationStrategy::Default.select_tmpls_and_flavors(tmpls, options)
   end
 
   private
 
   def unused_vms
-    # TODO ask PN for better query
     VirtualMachine.manageable.where('id NOT IN (SELECT DISTINCT(virtual_machine_id) FROM deployments)')
   end
 
