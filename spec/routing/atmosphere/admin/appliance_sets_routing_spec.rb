d9e007d9a322cad2de14f08d36d73f67f4207ce0
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-10-03 09:14:28 +0200

    routes spec are green

--- spec/routing/atmosphere/admin/appliance_sets_routing_spec.rb
+++ spec/routing/atmosphere/admin/appliance_sets_routing_spec.rb
@@ -1,27 +1,33 @@
-require "rails_helper"
+require 'rails_helper'
 
 describe Atmosphere::Admin::ApplianceSetsController do
-  describe "routing" do
+  routes { Atmosphere::Engine.routes }
 
-    it "routes to #index" do
-      expect(get("/admin/appliance_sets")).to route_to("admin/appliance_sets#index")
+  describe 'routing' do
+
+    it 'routes to #index' do
+      expect(get: '/admin/appliance_sets')
+        .to route_to('atmosphere/admin/appliance_sets#index')
     end
 
-    it "routes to #show" do
-      expect(get("/admin/appliance_sets/1")).to route_to("admin/appliance_sets#show", :id => "1")
+    it 'routes to #show' do
+      expect(get: '/admin/appliance_sets/1')
+        .to route_to('atmosphere/admin/appliance_sets#show', :id => '1')
     end
 
-    it "routes to #edit" do
-      expect(get("/admin/appliance_sets/1/edit")).to route_to("admin/appliance_sets#edit", :id => "1")
+    it 'routes to #edit' do
+      expect(get: '/admin/appliance_sets/1/edit')
+        .to route_to('atmosphere/admin/appliance_sets#edit', :id => '1')
     end
 
-    it "routes to #update" do
-      expect(put("/admin/appliance_sets/1")).to route_to("admin/appliance_sets#update", :id => "1")
+    it 'routes to #update' do
+      expect(put: '/admin/appliance_sets/1')
+        .to route_to('atmosphere/admin/appliance_sets#update', :id => '1')
     end
 
-    it "routes to #destroy" do
-      expect(delete("/admin/appliance_sets/1")).to route_to("admin/appliance_sets#destroy", :id => "1")
+    it 'routes to #destroy' do
+      expect(delete: '/admin/appliance_sets/1')
+        .to route_to('atmosphere/admin/appliance_sets#destroy', :id => '1')
     end
-
   end
 end
