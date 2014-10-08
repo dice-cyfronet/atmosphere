36b1b8e1e7394bc18883d84e9f307e361ab16ad5
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-10-08 14:55:47 +0200

    redis config from atmosphere module

--- config/initializers/4_sidekiq.rb
+++ config/initializers/4_sidekiq.rb
@@ -1,11 +1,14 @@
-redis_url = Air.config.sidekiq.url
-namespace = Air.config.sidekiq.namespace
-
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url, namespace: namespace }
+  config.redis = {
+    url: Atmosphere.redis_url,
+    namespace: Atmosphere.redis_namespace
+  }
   config.poll_interval = 1
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url, namespace: namespace }
+  config.redis = {
+    url: Atmosphere.redis_url,
+    namespace: Atmosphere.redis_namespace
+  }
 end
--- lib/atmosphere.rb
+++ lib/atmosphere.rb
@@ -40,4 +40,12 @@ module Atmosphere
 
     entities.merge(admin_entites_ext)
   end
+
+  # sidekiq redis url.
+  mattr_accessor :redis_url
+  @@redis_url = 'redis://localhost:6379'
+
+  # namespace for sidekiq delayed jobs.
+  mattr_accessor :redis_namespace
+  @@redis_namespace = 'atmosphere'
 end
