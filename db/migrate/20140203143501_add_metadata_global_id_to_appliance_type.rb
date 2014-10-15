bb6fa147e934ab08bf3c5122c6741f0032f00c60
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-10-15 11:09:01 +0200

    metadata_global_id is vphshare specific

--- db/migrate/20140203143501_add_metadata_global_id_to_appliance_type.rb
+++
@@ -1,7 +0,0 @@
-class AddMetadataGlobalIdToApplianceType < ActiveRecord::Migration
-  def change
-    change_table :atmosphere_appliance_types do |t|
-      t.column :metadata_global_id, :string, null: true
-    end
-  end
-end
