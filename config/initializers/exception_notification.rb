bb51ed41e0a0e49d61c67f59d4b46362ed54d7d6
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-10-17 13:19:15 +0200

    exp_notification and raven and project (e.g. vphshare) specific

--- config/initializers/exception_notification.rb
+++
@@ -1,8 +0,0 @@
-unless Air.config['exception_notification'].blank?
-  Air::Application.config.middleware.use ExceptionNotification::Rack,
-    email: {
-      email_prefix: Air.config.exception_notification.email_prefix,
-      sender_address: Air.config.exception_notification.sender_address,
-      exception_recipients: Air.config.exception_notification.exception_recipients
-    }
-end
--- config/initializers/raven.rb
+++
@@ -1,7 +0,0 @@
-if Air.config['raven_dsn']
-  require 'raven'
-
-  Raven.configure do |config|
-    config.dsn = Air.config.raven_dsn
-  end
-end
