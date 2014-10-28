a734806bc01ee435db4feaa61d5b2da8e8c03b42
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-10-28 12:46:48 +0100

    add informaction about all pmt

--- spec/serializers/clew_appliance_instances_serializer_spec.rb
+++ spec/serializers/clew_appliance_instances_serializer_spec.rb
@@ -23,4 +23,19 @@ describe ClewApplianceInstancesSerializer do
 
     return as, http_m
   end
+
+  it 'returns information about all port mapping templates' do
+    at = create(:appliance_type)
+    pmt = create(:port_mapping_template, appliance_type: at)
+    as = create(:appliance_set, appliance_set_type: :portal)
+    appl = create(:appliance, appliance_type: at, appliance_set: as)
+    serializer = ClewApplianceInstancesSerializer.new(appliance_sets: [as])
+
+    result = JSON.parse(serializer.to_json)
+    appl = result['clew_appliance_instances']['appliances'].first
+    port_mapping_templates = appl['port_mapping_templates']
+
+    expect(port_mapping_templates.size).to eq 1
+    expect(port_mapping_templates.first['id']).to eq pmt.id
+  end
 end
