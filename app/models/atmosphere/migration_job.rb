da09e3c74f90b1b9368b65c6d21f2f311c573eec
Paweł Suder <pawel@suder.info>
2014-11-21 12:32:00 +0100

    Add migration virtual machine templates model, view and controller

---
+++ app/models/atmosphere/migration_job.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: migration_jobs
+#
+#  id                          :integer          not null, primary key
+#  appliance_type_id           :integer
+#  compute_site_source_id      :integer
+#  compute_site_destination_id :integer
+#  status                      :text
+#  created_at                  :datetime
+#  updated_at                  :datetime
+#  virtual_machine_template_id :integer
+#
+
+module Atmosphere
+  class MigrationJob < ActiveRecord::Base
+    belongs_to :appliance_type
+    belongs_to :virtual_machine_template
+    belongs_to :compute_site_source, class_name: 'ComputeSite', foreign_key: 'compute_site_source_id'
+    belongs_to :compute_site_destination, class_name: 'ComputeSite', foreign_key: 'compute_site_destination_id'
+
+    default_scope { order(updated_at: :desc) }
+  end
+end
---
+++ app/services/atmosphere/vmt_migrator.rb
@@ -0,0 +1,29 @@
+module Atmosphere
+  class VmtMigrator
+    def initialize(virtual_machine_template, source_compute_site, destination_compute_site)
+      @virtual_machine_template = virtual_machine_template
+      @source_compute_site = source_compute_site
+      @destination_compute_site = destination_compute_site
+    end
+
+    def execute()
+      if @source_compute_site.technology == 'openstack'
+        migrator_class = nil
+
+        case @destination_compute_site.technology
+        when 'aws'
+          migrator_class = Migration::Worker::OpenstackAmazonMigrator
+        when 'openstack'
+          migrator_class = Migration::Worker::OpenstackOpenstackMigrator
+        end
+
+        if migrator_class != nil
+          Sidekiq::Client.push(
+            'queue' => "migration_#{@source_compute_site.site_id}",
+            'class' => migrator_class,
+            'args' => [@virtual_machine_template.id_at_site, @destination_compute_site.site_id])
+        end
+      end
+    end
+  end
+end
