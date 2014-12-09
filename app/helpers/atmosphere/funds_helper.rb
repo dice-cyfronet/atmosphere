98a5873e0458e2957f04fc4f91c4d7b98c954a53
gubala <tg.errors@gmail.com>
2014-12-09 11:55:22 +0100

    slightly better balance helper and specs

--- app/helpers/atmosphere/funds_helper.rb
+++ app/helpers/atmosphere/funds_helper.rb
@@ -2,7 +2,8 @@ module Atmosphere
   module FundsHelper
 
     def fund_balance_full_precision(balance)
-      "#{'  -'[balance <=> 0]}#{balance.abs / 10000}." +
+      balance = balance.to_i
+      "#{'-' if balance < 0}#{balance.abs / 10000}." +
       "#{(balance.abs % 10000).to_s.rjust(4, '0')}"
     end
 
