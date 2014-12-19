ac95fb3b13db9cc3983fe551916de72ce516ef77
Bartosz Wilk <b.wilk@cyfronet.pl>
2014-12-19 10:46:09 +0100

    Scaling tested

--- spec/services/atmosphere/optimizer_spec.rb
+++ spec/services/atmosphere/optimizer_spec.rb
@@ -569,4 +569,103 @@ describe Atmosphere::Optimizer do
       end
     end
   end
+
+  context 'scaling', focus: true do
+    let(:strategy) { double() }
+    let(:appliance) { double(optimization_strategy: strategy) }
+
+    let(:appl_vm_manager) { double('appliance_vms_manager') }
+
+    before do
+
+      allow(Atmosphere::ApplianceVmsManager).to receive(:new).and_return(appl_vm_manager)
+
+
+    end
+
+    context 'cannot scale manually' do
+
+      it 'blocks manual scaling' do
+
+        detail = {}
+        detail[:appliance] = appliance
+        detail[:quantity] = 2
+
+        hint = {}
+        hint[:scaling] = detail
+
+        expect(strategy).to receive(:can_scale_manually?).and_return(false)
+        expect(appl_vm_manager).to receive(:unsatisfied)
+        expect(appl_vm_manager).to receive(:save)
+
+        subject.run(hint)
+
+
+      end
+    end
+
+    context 'can scale manually' do
+
+      before do
+        allow(strategy).to receive(:can_scale_manually?).and_return(true)
+      end
+
+      let(:vms) {double('vms')}
+
+
+      it 'runs scaling up' do
+
+        detail = {}
+        detail[:appliance] = appliance
+        detail[:quantity] = 2
+
+        hint = {}
+        hint[:scaling] = detail
+
+        expect(strategy).to receive(:vms_to_start).with(appliance, detail[:quantity]).and_return(vms)
+        expect(appl_vm_manager).to receive(:start_vms!).with(vms)
+        expect(appl_vm_manager).to receive(:save)
+
+        subject.run(hint)
+
+      end
+
+      it 'runs scaling down succesfully' do
+
+        detail = {}
+        detail[:appliance] = appliance
+        detail[:quantity] = -2
+
+        hint = {}
+        hint[:scaling] = detail
+
+        expect(strategy).to receive(:vms_to_stop).with(appliance, -detail[:quantity]).and_return(vms)
+        expect(vms).to receive(:count).and_return(2)
+        expect(appl_vm_manager).to receive(:stop_vms!).with(vms)
+        expect(appl_vm_manager).to receive(:save)
+
+        subject.run(hint)
+
+      end
+
+      it 'runs scaling down without enogh vms' do
+
+        detail = {}
+        detail[:appliance] = appliance
+        detail[:quantity] = -2
+
+        hint = {}
+        hint[:scaling] = detail
+
+        expect(strategy).to receive(:vms_to_stop).with(appliance, -detail[:quantity]).and_return(vms)
+        expect(vms).to receive(:count).and_return(1)
+        expect(appl_vm_manager).to receive(:unsatisfied)
+        expect(appl_vm_manager).to receive(:save)
+
+        subject.run(hint)
+
+      end
+    end
+  end
+
 end
