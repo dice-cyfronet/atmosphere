e7b8f9b9e26ca6dc48d38bf6c08a1f4994ae0fe2
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-12-29 13:36:32 +0100

    don't repeat similar code

--- app/serializers/atmosphere/appliance_type_serializer.rb
+++ app/serializers/atmosphere/appliance_type_serializer.rb
@@ -6,31 +6,35 @@ module Atmosphere
     include Atmosphere::ApplianceTypeSerializerExt
     embed :ids
 
-    attributes :id, :name, :description, :shared, :scalable, :visible_to, :author_id
+    attributes :id, :name, :description,
+               :shared, :scalable, :visible_to, :author_id
     attributes :preference_cpu, :preference_memory, :preference_disk
     attributes :active, :saving
 
-    has_many :appliances, :port_mapping_templates, :appliance_configuration_templates, :virtual_machine_templates, :compute_sites
+    has_many :appliances, :port_mapping_templates,
+             :appliance_configuration_templates,
+             :virtual_machine_templates,
+             :compute_sites
+
+    private
 
     def author_id
       object.user_id
     end
 
     def active
-      active_vmts = object.virtual_machine_templates
-      .joins(:compute_site)
-      .where(state: :active, atmosphere_compute_sites: { active: true })
-
-      active_vmts.count > 0
+      vmt_with_state?(:active)
     end
 
     def saving
-      saving_vms = object.virtual_machine_templates
-      .where(state: :saving)
-
-      saving_vms.count > 0
+      vmt_with_state?(:saving)
     end
 
+    def vmt_with_state?(state)
+      object.virtual_machine_templates.
+        on_active_cs.where(state: state).
+          count > 0
+    end
 
     def compute_sites
       object.compute_sites.active
