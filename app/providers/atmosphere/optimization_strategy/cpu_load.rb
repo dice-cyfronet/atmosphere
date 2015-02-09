852b61a2f324c1dc5d0ec317c3e0327c62c0f377
tbartynski <tomek.bartynski@gmail.com>
2015-02-09 10:15:25 +0100

    Prototype impl of CPU load based opt strategy
    https://github.com/dice-cyfronet/atmosphere/pull/101

---
+++ app/providers/atmosphere/optimization_strategy/cpu_load.rb
@@ -0,0 +1,21 @@
+module Atmosphere
+  module OptimizationStrategy
+    class CpuLoad < OptimizationStrategy::Default
+      def monitor_vms?
+        true
+      end
+
+      def handle_event(hash)
+        nil
+      end
+
+      def event_definitions
+        {
+          simple_event: {name: 'CpuLoad', properties: {}},
+          complex_event: ''
+        }
+      end
+
+    end
+  end
+end
