bce7cfe912da81cd67cbaba668fc4014d5e32e3c
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-04-14 18:01:10 +0200

    Create admin in development and production envs

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -1,2 +1,14 @@
 Atmosphere::OSFamily.find_or_create_by(name: 'Windows')
 Atmosphere::OSFamily.find_or_create_by(name: 'Linux')
+
+unless Rails.env.test?
+  Atmosphere::User.find_or_initialize_by(login: 'admin').tap do |u|
+    u.full_name = 'Admin'
+    u.email = 'admin@example.com'
+    u.password = 's3cr3t!!!'
+    u.password_confirmation = 's3cr3t!!!'
+    u.roles = [:admin, :developer]
+
+    u.save!
+  end
+end
