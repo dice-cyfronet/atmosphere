7577b60a4edb090427289575177ae3da8f508660
Tomasz Gubała <t.gubala@cyfronet.pl>
2015-04-21 14:13:46 +0200

    barking hound fix

--- app/models/atmosphere/compute_site.rb
+++ app/models/atmosphere/compute_site.rb
@@ -125,7 +125,10 @@ module Atmosphere
 
     scope :active, -> { where(active: true) }
 
-    scope :funded_by, ->(fund) { joins(:funds).where(atmosphere_funds: { id: fund.id }) }
+    scope :funded_by, ->(fund) do
+      joins(:funds)
+        .where(atmosphere_funds: { id: fund.id })
+    end
 
     after_update :update_cloud_client, if: :config_changed?
     after_destroy :unregister_cloud_client
