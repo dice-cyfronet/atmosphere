c4d9c3988a27eea2233bae9ea2ec860deffa45c3
Tomasz Gubała <t.gubala@cyfronet.pl>
2015-05-05 16:45:49 +0200

    validation info for user

--- app/controllers/atmosphere/admin/virtual_machine_flavors_controller.rb
+++ app/controllers/atmosphere/admin/virtual_machine_flavors_controller.rb
@@ -8,15 +8,21 @@ class Atmosphere::Admin::VirtualMachineFlavorsController < Atmosphere::Admin::Ap
 
   # PATCH/PUT /virtual_machine_flavors/1
   def update
+    alert = nil
     params[:virtual_machine_flavor][:hourly_cost].each do |os_family_id, cost|
       os_family = Atmosphere::OSFamily.find_by(id: os_family_id)
-      if cost.strip.empty? && os_family
-        @virtual_machine_flavor.remove_hourly_cost_for(os_family)
+      if cost.strip.empty? &&
+          os_family &&
+          @virtual_machine_flavor.get_hourly_cost_for(os_family)
+        unless @virtual_machine_flavor.remove_hourly_cost_for(os_family)
+          alert = I18n.t('virtual_machine_flavor.cant_destroy',
+                         os_family: os_family.name)
+        end
       elsif cost.strip.present? && cost.to_i >= 0  && os_family
         @virtual_machine_flavor.set_hourly_cost_for(os_family, cost.to_i)
       end
     end
-    redirect_to admin_compute_sites_path
+    redirect_to admin_compute_sites_path, alert: alert
   end
 
   private
