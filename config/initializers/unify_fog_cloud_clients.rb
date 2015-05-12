b28cca0d51b93385ca9134c3b56f7a7c51943fe2
tbartynski <tomek.bartynski@gmail.com>
2015-05-12 10:36:23 +0200

    monkey pathing azure-sdk
    https://redmine.dev.cyfronet.pl/issues/3335

--- config/initializers/unify_fog_cloud_clients.rb
+++ config/initializers/unify_fog_cloud_clients.rb
@@ -211,9 +211,66 @@ module Azure::VirtualMachineManagement::Serialization
   end
   def self.deployment_to_xml(params, options)
     xml = self.deployment_to_xml_orig(params, options)
-    logger.info "===========XML=============="
-    logger.info xml
+    puts "===========XML=============="
+    puts xml
     xml
+  end
+
+  def self.role_to_xml(params, options)
+    puts "params: #{params}"
+    puts "options: #{options}"
+    img_is_user_created = user_image? params[:image]
+    builder = Nokogiri::XML::Builder.new do |xml|
+      xml.PersistentVMRole(
+        'xmlns' => 'http://schemas.microsoft.com/windowsazure',
+        'xmlns:i' => 'http://www.w3.org/2001/XMLSchema-instance'
+      ) do
+        xml.RoleName { xml.text params[:vm_name] }
+        xml.OsVersion('i:nil' => 'true')
+        xml.RoleType 'PersistentVMRole'
+
+        xml.ConfigurationSets do
+          provisioning_configuration_to_xml(xml, params, options) unless img_is_user_created
+          xml.ConfigurationSet('i:type' => 'NetworkConfigurationSet') do
+            xml.ConfigurationSetType 'NetworkConfiguration'
+            xml.InputEndpoints do
+              default_endpoints_to_xml(xml, options)
+              tcp_endpoints_to_xml(
+                xml,
+                options[:tcp_endpoints],
+                options[:existing_ports]
+              ) if options[:tcp_endpoints]
+            end
+            if options[:virtual_network_name] && options[:subnet_name]
+              xml.SubnetNames do
+                xml.SubnetName options[:subnet_name]
+              end
+              xml.StaticVirtualNetworkIPAddress options[:static_virtual_network_ipaddress] if options[:static_virtual_network_ipaddress]
+            end
+          end
+        end
+        xml.AvailabilitySetName options[:availability_set_name]
+        # Label does not seem to be required
+        #xml.Label Base64.encode64(params[:vm_name]).strip
+
+        xml.VMImageName params[:image] if img_is_user_created
+
+        xml.OSVirtualHardDisk do
+          xml.MediaLink 'http://' + options[:storage_account_name] + '.blob.core.windows.net/vhds/' + (Time.now.strftime('disk_%Y_%m_%d_%H_%M_%S_%L')) + '.vhd'
+          xml.SourceImageName params[:image] unless img_is_user_created
+        end
+
+        xml.RoleSize options[:vm_size] || params[:vm_size]
+      end
+    end
+    builder.doc
+  end
+
+  private
+  def self.user_image?(image_id)
+    img_svc = Azure::VirtualMachineImageManagementService.new
+    img = img_svc.list_virtual_machine_images.select { |i| i.name == image_id}.first
+    img.category == 'User'
   end
 end
 
@@ -226,7 +283,7 @@ module Azure
         os_images = []
         virtual_machine_images = imageXML.css('VMImages VMImage')
         virtual_machine_images.each do |image_node|
-          image = ::Azure::VirtualMachineImageManagement::VirtualMachineImage.new
+          image = Azure::VirtualMachineImageManagement::VirtualMachineImage.new
           image.os_type = xml_content(image_node, 'OSDiskConfiguration OS')
           image.name = xml_content(image_node, 'Name')
           image.category = xml_content(image_node, 'Category')
