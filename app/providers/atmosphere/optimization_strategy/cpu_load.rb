ea5831bb8c2f54996d3c3fab31207e2c83308984
tomek <tomek.bartynski@gmail.com>
2015-08-03 15:12:58 +0200

    prototyping load based optimization strategy

--- app/providers/atmosphere/optimization_strategy/cpu_load.rb
+++ app/providers/atmosphere/optimization_strategy/cpu_load.rb
@@ -6,14 +6,29 @@ module Atmosphere
       end
 
       def handle_event(hash)
-        nil
+        logger.info "Received complex event: #{hash}"
       end
 
       def event_definitions
+        time_window = 300
+        interval = 600
         {
-          simple_event: { name: 'CpuLoad', properties: {} },
-          complex_event: ''
+          simple_event: {
+            name: 'CpuLoad',
+            properties: {
+              'vmUuid': 'String',
+              'cpuLoad': 'float'
+            }
+          },
+          complex_event:
+            "select avg(cpuLoad), vmUuid"\
+            " from CpuLoad.win:time(#{time_window} sec)"\
+            " having avg(cpuLoad) > 0.8 output first every #{interval} seconds"
         }
+      end
+      private
+      def logger
+        Atmosphere.complex_event_logger
       end
     end
   end
