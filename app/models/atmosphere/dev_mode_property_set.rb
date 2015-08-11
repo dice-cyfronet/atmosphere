7fe0298e5d2a7728f1eff563b285aecc5fc852d6
tomek <tomek.bartynski@gmail.com>
2015-08-11 16:37:39 +0200

    added helper method to get id of associated user
    https://github.com/dice-cyfronet/atmosphere/issues/219

--- app/models/atmosphere/dev_mode_property_set.rb
+++ app/models/atmosphere/dev_mode_property_set.rb
@@ -62,6 +62,10 @@ module Atmosphere
                 allow_nil: true
               }
 
+    def user_id
+      (appliance && appliance.appliance_set && appliance.appliance_set.user_id) || nil
+    end
+
     def self.create_from(appliance_type)
       copy_params = [
         'name', 'description', 'shared',
