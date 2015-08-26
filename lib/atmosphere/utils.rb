5dd338349badfa116fa1ae6f09c71e6b2f13e6b8
tomek <tomek.bartynski@gmail.com>
2015-08-26 09:51:20 +0200

    methods do not fail if given block returns nil
    https://github.com/dice-cyfronet/atmosphere/issues/125

--- lib/atmosphere/utils.rb
+++ lib/atmosphere/utils.rb
@@ -1,43 +1,100 @@
 module Atmosphere
   module Utils
-    def min_elements_by(arr)
-      min_elements = [arr.shift]
-      arr.each do |e|
-        if block_given?
-          if yield(min_elements.first) > yield(e)
-            min_elements = [e]
-          elsif yield(min_elements.first) == yield(e)
-            min_elements << e
-          end
-        else
-          if min_elements.first > e
-            min_elements = [e]
-          elsif min_elements.first == e
-            min_elements << e
-          end
-        end
+    def min_elements_by(arr, &block)
+      if block_given?
+        min_elements_by_with_block(arr, &block)
+      else
+        min_elements_by_without_block(arr)
       end
-      min_elements.compact
     end
 
-    def max_elements_by(arr)
-      max_elements = [arr.shift]
+    def max_elements_by(arr, &block)
+      if block_given?
+        max_elements_by_with_block(arr, &block)
+      else
+        max_elements_by_without_block(arr)
+      end
+    end
+
+    private
+
+    def min_elements_by_with_block(arr, &block)
+      min_elements, arr = first_elements_and_residue(arr, &block)
+      return [] if arr.empty? && min_elements.nil?
       arr.each do |e|
-        if block_given?
-          if yield(max_elements.first) < yield(e)
-            max_elements = [e]
-          elsif yield(max_elements.first) == yield(e)
-            max_elements << e
-          end
-        else
-          if max_elements.first < e
-            max_elements = [e]
-          elsif max_elements.first == e
-            max_elements << e
-          end
+        current_min_elements_value = yield(min_elements.first)
+        element_value = yield(e)
+        next unless current_min_elements_value && element_value
+
+        if current_min_elements_value > element_value
+          min_elements = [e]
+        elsif current_min_elements_value == element_value
+          min_elements << e
         end
       end
-      max_elements.compact
+      min_elements
+    end
+
+    def first_elements_and_residue(arr)
+      arr.compact!
+      return [nil, []] if arr.empty?
+      first_el = arr.shift
+
+      until yield(first_el) || arr.empty?
+        first_el = arr.shift
+      end
+      first_elements = if yield(first_el).nil?
+                         []
+                       else
+                         [first_el]
+                       end
+      [first_elements, arr]
+
+    end
+
+    def min_elements_by_without_block(arr)
+      arr.compact!
+      return [] if arr.empty?
+      min_elements = [arr.shift]
+      arr.each do |e|
+        if min_elements.first > e
+          min_elements = [e]
+        elsif min_elements.first == e
+          min_elements << e
+        end
+      end
+      min_elements
+    end
+
+    def max_elements_by_with_block(arr, &block)
+      max_elements, arr = first_elements_and_residue(arr, &block)
+      return [] if arr.empty? && max_elements.nil?
+      arr.each do |e|
+        current_max_elements_value = yield(max_elements.first)
+        element_value = yield(e)
+        next unless current_max_elements_value && element_value
+
+        if current_max_elements_value < element_value
+          max_elements = [e]
+        elsif current_max_elements_value == element_value
+          max_elements << e
+        end
+      end
+      max_elements
+    end
+
+    def max_elements_by_without_block(arr)
+      arr.compact!
+      return [] if arr.empty?
+      max_elements = [arr.shift]
+      arr.each do |e|
+        if max_elements.first < e
+          max_elements = [e]
+        elsif max_elements.first == e
+          max_elements << e
+        end
+      end
+      max_elements
     end
   end
 end
--- spec/lib/atmosphere/utils_spec.rb
+++ spec/lib/atmosphere/utils_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 require 'atmosphere/utils'
-#$: << '../../lib'
+# $: << '../../lib'
 
 describe Atmosphere::Utils do
   include Atmosphere::Utils
@@ -14,11 +14,15 @@ describe Atmosphere::Utils do
       end
 
       it 'returns array with all min input values' do
-        expect(min_elements_by([2,3,1,1,5,3])).to eq [1,1]
+        expect(min_elements_by([2, 3, 1, 1, 5, 3])).to eq [1, 1]
       end
 
       it 'returns array with one elements if there is one min value in input' do
         expect(min_elements_by([2,3,4,1,4])).to eq [1]
+      end
+
+      it 'excludes nil elements' do
+        expect(min_elements_by([2, nil, 3, 4, 1, 4])).to eq [1]
       end
 
     end
@@ -26,15 +30,33 @@ describe Atmosphere::Utils do
     context 'called with block' do
 
       it 'return empty array for empty input when block given' do
-        expect(min_elements_by([]) {|e| e.length}).to be_empty
+        expect(min_elements_by([]) { |e| e.length }).to be_empty
+      end
+
+      it 'returns first element if array size is one' do
+        expect(min_elements_by([1]) { |e| e }).to eq [1]
       end
 
       it 'returns array with all min input values' do
-        expect(min_elements_by(['a', 'aa', 'b']) {|e| e.length}).to eq ['a','b']
+        expect(min_elements_by(['a', 'aa', 'b']) { |e| e.length }).
+          to eq ['a', 'b']
       end
 
       it 'returns array with one elements if there is one min value in input' do
         expect(min_elements_by(['ae', 'v', 'qqq']) {|e| e.length}).to eq ['v']
+      end
+
+      it 'does not raise error if block returns nil' do
+        min_elements_by([1, 2, 3,]) { |e| e == 2 ? nil : e }
+      end
+
+      it 'excludes elements for which block return nil' do
+        expect(min_elements_by([7, 2, 3,]) { |e| e == 2 ? nil : e }).to eq [3]
+        expect(min_elements_by([5]) { nil }).to be_empty
+      end
+
+      it 'returns empty array if block returns nil for all elements' do
+        expect(min_elements_by([1]) { nil }).to eq []
       end
 
     end
@@ -49,11 +71,15 @@ describe Atmosphere::Utils do
       end
 
       it 'returns array with all max input values' do
-        expect(max_elements_by([2,3,5,1,5,3])).to eq [5,5]
+        expect(max_elements_by([2, 3, 5, 1, 5, 3])).to eq [5, 5]
       end
 
       it 'returns array with one elements if there is one max value in input' do
-        expect(max_elements_by([2,3,7,1,4])).to eq [7]
+        expect(max_elements_by([2, 3, 7, 1, 4])).to eq [7]
+      end
+
+      it 'excludes nil elements' do
+        expect(max_elements_by([2, nil, 3, 4, 1, 4])).to eq [4, 4]
       end
 
     end
@@ -65,11 +91,24 @@ describe Atmosphere::Utils do
       end
 
       it 'returns array with all max input values' do
-        expect(max_elements_by(['a', 'aa', 'b', 'bb']) {|e| e.length}).to eq ['aa','bb']
+        expect(max_elements_by(['a', 'aa', 'b', 'bb']) { |e| e.length}).
+          to eq ['aa', 'bb']
       end
 
       it 'returns array with one elements if there is one max value in input' do
         expect(max_elements_by(['ae', 'v', 'qqq']) {|e| e.length}).to eq ['qqq']
+      end
+
+      it 'does not raise error if block returns nil' do
+        max_elements_by([1, 2, 3,]) { |e| e == 2 ? nil : e }
+      end
+
+      it 'excludes elements for which block return nil' do
+        expect(max_elements_by([7, 2, 3,]) { |e| e == 2 ? nil : e }).to eq [7]
+      end
+
+      it 'returns empty array if block returns nil for all elements' do
+        expect(max_elements_by([1]) { nil }).to eq []
       end
 
     end
