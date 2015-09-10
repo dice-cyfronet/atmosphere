db31cae3241080d1a4a6276a583e5fdb1f0c7440
Piotr Nowakowski <ymnowako@cyf-kr.edu.pl>
2015-09-10 12:41:47 +0200

    Isolate render_json_error as separate module

--- app/controllers/atmosphere/api/application_controller.rb
+++ app/controllers/atmosphere/api/application_controller.rb
@@ -1,6 +1,9 @@
 module Atmosphere
   module Api
     class ApplicationController < ::ApplicationController
+
+      include Atmosphere::JsonErrorHandler
+
       protect_from_forgery with: :null_session, if: :token_request?
       protect_from_forgery with: :exception, unless: :token_request?
 
@@ -51,18 +54,18 @@ module Atmosphere
                           details: model_obj.errors)
       end
 
-      def render_json_error(msg, options = {})
-        error_json = {
-          message: msg,
-          type: options[:type] || :general
-        }
-        error_json[:details] = options[:details] if options[:details]
-
-        render(
-          json: error_json,
-          status: options[:status] || :bad_request
-        )
-      end
+      # def render_json_error(msg, options = {})
+      #   error_json = {
+      #     message: msg,
+      #     type: options[:type] || :general
+      #   }
+      #   error_json[:details] = options[:details] if options[:details]
+      #
+      #   render(
+      #     json: error_json,
+      #     status: options[:status] || :bad_request
+      #   )
+      # end
 
       def load_all?
         admin? && params['all']
