28220f6f0851e7784d797d55ca35f0bf963cf6ae
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-10-28 10:06:49 +0100

    Revert "Better way to check when add engine migrations"
    This reverts commit 9c4a8441c94241480726650acd9576ec62722eb6.

--- lib/atmosphere/engine.rb
+++ lib/atmosphere/engine.rb
@@ -38,9 +38,8 @@ module Atmosphere
       g.helper false
     end
 
-
     initializer :append_migrations do |app|
-      if app.root.to_s != root.to_s
+      unless app.root.to_s.match root.to_s
         config.paths['db/migrate'].expanded.each do |expanded_path|
           app.config.paths['db/migrate'] << expanded_path
         end
