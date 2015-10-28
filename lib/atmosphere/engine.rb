41b04c0d505d446e522579806613e8f307799bfe
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-10-28 10:25:59 +0100

    Check if app dir includes root dir while adding engine migrations
    The problem exists when following dirs were used:
      - atmosphere dir: `/home/marek/git/atmosphere`
      - application dir: `/home/marek/git/atmosphere-vph`
    Now we are adding `/` at the end of atmosphere dir - it solved the
    problem described above.

--- lib/atmosphere/engine.rb
+++ lib/atmosphere/engine.rb
@@ -39,7 +39,7 @@ module Atmosphere
     end
 
     initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
+      unless app.root.to_s.match("#{root}/")
         config.paths['db/migrate'].expanded.each do |expanded_path|
           app.config.paths['db/migrate'] << expanded_path
         end
