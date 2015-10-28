9c4a8441c94241480726650acd9576ec62722eb6
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-10-28 09:41:57 +0100

    Better way to check when add engine migrations

--- lib/atmosphere/engine.rb
+++ lib/atmosphere/engine.rb
@@ -38,8 +38,9 @@ module Atmosphere
       g.helper false
     end
 
+
     initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
+      if app.root.to_s != root.to_s
         config.paths['db/migrate'].expanded.each do |expanded_path|
           app.config.paths['db/migrate'] << expanded_path
         end
